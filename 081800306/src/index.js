84f7592a095f82bcc067936059440b3aa25fde13
czj6 <673330108@qq.com>
2021-02-25 22:05:37 +0800

    正则表达式‘

---
+++ 081800306/src/index.js
@@ -0,0 +1,66 @@
+const fs = require('fs')
+// let file = process.argv[2]
+const han = /[\u4e00-\u9fa5]+/ //匹配汉字的正则
+const symbol = /[\n\r]+/
+const asciiRex = /[\x00-\xff]+/
+const wordRex = /[A-Za-z]{4,}[\'\-]?[A-Za-z0-9]*/gi
+let wordCount = 0
+let lineBreaks = 0
+let rows = 0
+let data = fs.readFileSync('a.txt',{encoding: 'utf8'})
+let arr = data.split('')
+arr = arr.filter(word => {
+  // return !han.test(word)
+  // if(asciiRex.test(word)){
+  //   console.log(word);
+  // }
+  if(symbol.test(word)){
+    lineBreaks++
+  }
+  return !han.test(word) && !symbol.test(word) && asciiRex.test(word)
+})
+let res = arr.join('')
+let buffer = Buffer.from(data,'ascii')
+let str = res.match(wordRex)
+rows = lineBreaks/2 + 1
+wordCount = arr.length + rows-1
+console.log(countNum(str)); 
+
+/**
+ *  在ascii编码中，汉字一个字节，换行符两个字节，标点符号一个字节，水平制表符
+ */
+
+ /**
+  * 
+  * @param  arr: Array 
+  */
+ 
+function countNum(arr){
+  let obj = {
+  }
+  arr.sort()
+  var proxy = new Proxy(obj,{
+    get: function(obj,key){
+      if(key in obj){
+        return obj[key]
+      }else{
+        return 0
+      }
+    }
+  })
+  arr.forEach( item => {
+    proxy[item]++;
+  });
+  let data = []
+  for (const key in obj) {
+    let o = {}
+    o[key] = obj[key]
+    data.push(o)
+  }
+  data.sort(function(a,b){
+    let key1 = Object.keys(a)[0]
+    let key2 = Object.keys(b)[0]
+    return b[key2] - a[key1]
+  })
+  console.log(data.slice(0,3));
+}
