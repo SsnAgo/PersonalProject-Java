1e24818325c072ffd387a58df0ba2b1fc32c252b
DumbassL <1833962756@qq.com>
2021-02-25 20:30:30 +0800

    第二次commit 具体内容在README.md中

--- 221801401/src/Lib.java
+++ 221801401/src/Lib.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /*
  * @author 张海浪
@@ -7,51 +9,113 @@ import java.util.*;
  * 
  * */
 public class Lib {
-	private static String WORD_REGEX="[a-z]{4}[a-z0-9]*";
-	private static String FLITER_REGEX="[^a-zA-Z0-9]";
-	
-	//获取当前工程的路径
-	public static String DIR=System.getProperty("user.dir");
-	
-	/*
-	 * 统计文件行数
-	 * @param file
-	 * @return lines
-	 * */
-	public static int getTextLines(String filePath) {
-		//行数的统计量
-		int count=0;
-		//循环变量
-		String str="";
-		
-		//得到输入流
-		FileInputStream is=null;
-		InputStreamReader isr=null;
-		BufferedReader br=null;
-		
-		try {
-			is=new FileInputStream(filePath);
-			isr=new InputStreamReader(is);
-			br=new BufferedReader(isr);
-			while((str=br.readLine())!=null) {
-				if(!"".equals(str)) {
-					count++;
-				}
-			}
-		}catch(FileNotFoundException e) {
-			e.printStackTrace();
-		}catch(IOException e) {
-			e.printStackTrace();
-		}finally {
-			try {
-				//关闭输入流
-				is.close();
-				isr.close();
-				br.close();
-			}catch(IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return count;
-	}
+    private static String CHINESE_RE="[\\u4e00-\\u9fa5]";
+    
+    //获取当前工程的路径
+    public static String DIR=System.getProperty("user.dir");
+    
+    
+    /*
+     * 用正则表达式CHINESE_RE过滤中文
+     * @param str
+     * @return newStr
+     * */
+    public static String filterChinese(String str) {
+        Pattern pattern=Pattern.compile(CHINESE_RE);
+        Matcher matcher=pattern.matcher(str);
+        String newStr=matcher.replaceAll("");
+        return newStr;
+    }
+    
+    
+    /*
+     * 统计文件行数
+     * @param filePath
+     * @return count
+     * */
+    public static int getLineCount(String filePath) {
+	    //行数的统计量
+	    int count=0;
+	    //循环变量
+	    String str="";
+	    
+	    //得到输入流
+	    FileInputStream is=null;
+	    InputStreamReader isr=null;
+	    BufferedReader br=null;
+	    
+	    try {
+		    is=new FileInputStream(filePath);
+		    isr=new InputStreamReader(is);
+		    br=new BufferedReader(isr);
+		    while((str=br.readLine())!=null) {
+			    if(!"".equals(str)) {
+				    count++;
+			    }
+		    }
+	    }catch(FileNotFoundException e) {
+		    e.printStackTrace();
+        }catch(IOException e) {
+		    e.printStackTrace();
+	    }finally {
+		    try {
+			    //关闭输入流
+			    is.close();
+			    isr.close();
+			    br.close();
+		    }catch(IOException e) {
+			    e.printStackTrace();
+		    }
+	    }
+	    return count;
+    }
+    
+    
+    /*
+     * 统计文件的字符数
+     * @ filePath
+     * @ return count
+     * */
+    public static int getCharactersCount(String filePath) {
+        //字符数的统计量
+        int count=0;
+        //循环变量
+        String temp="";
+        
+        //得到输入流
+	    FileInputStream is=null;
+	    InputStreamReader isr=null;
+	    BufferedReader br=null;
+	    try {
+	        is=new FileInputStream(filePath);
+	        isr=new InputStreamReader(is);
+	        br=new BufferedReader(isr);
+	        while((temp=br.readLine())!=null) {
+	            if(!"".equals(temp)) {
+	                char[] chars=temp.toCharArray();
+	                for(int i=0;i<chars.length;i++) {
+	                    if(chars[i]>=0&&chars[i]<=127) {
+	                        count++;
+	                    }
+	                }
+	                count++;
+	            }
+	        }
+	        count--;
+	    }catch(FileNotFoundException e) {
+	        e.printStackTrace();
+	    }catch(IOException e) {
+	        e.printStackTrace();
+	    }finally {
+	        try {
+	            //关闭输入流
+	            is.close();
+	            isr.close();
+	        }catch(IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    return count;
+    }
+    
 }
--- 221801401/src/WordCount.java
+++ 221801401/src/WordCount.java
@@ -2,7 +2,8 @@ import java.io.IOException;
 
 public class WordCount {
 	public static void main(String[] args) throws IOException {
-		System.out.println(Lib.getTextLines("D://test.txt"));
+		System.out.println(Lib.getLineCount("D://test.txt"));
 		System.out.println(Lib.DIR);
+		System.out.println(Lib.getCharactersCount("D://test.txt"));
 	}
 }
