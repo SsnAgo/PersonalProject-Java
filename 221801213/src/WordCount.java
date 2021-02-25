0ca12f13dd065e662947ae3c36ca05e510b71be1
Guodd-1 <1563854942@qq.com>
2021-02-25 17:22:42 +0800

    Update WordCount.java
    test

---
+++ 221801213/src/WordCount.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+import java.util.*;
+
+public class WordCount
+{
+	public WordCount()
+	{
+		System.out.println("hellow world");
+	}
+	
+	public static void main(String args[])
+	{
+		WordCount wc=new WordCount();
+	}
+}
+
