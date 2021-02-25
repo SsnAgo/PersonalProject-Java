739267d9ec286c43c088ec408e8bce272eabeed7
BobbyShao <79575907+BobbyShao@users.noreply.github.com>
2021-02-25 15:04:05 +0800

    实现行数统计的功能

--- 221801301/src/WordCount.java
+++ 221801301/src/WordCount.java
@@ -1,50 +1,46 @@
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.LineNumberReader;
 
 public class WordCount {
 	public static void main(String[] args) throws IOException{
-			int i;
-			FileInputStream fin;
-			FileOutputStream fout;
-
-			try{
-			//open input file
-			try{
-			fin=new FileInputStream(args[0]);
-			}
-			catch(FileNotFoundException exc){
-			System.out.println("Input file not found!");
-			return;
-			}
-
-			//open output file
-			try{
-			fout=new FileOutputStream(args[1]);
-			}
-			catch(FileNotFoundException exc){
-			System.out.println("Error opening Output file!");
-			return;
-			}
-			}
-			catch(ArrayIndexOutOfBoundsException exc){
-			System.out.println("Usage:CopyFile From To");
-			return;
-			}
-
-			//copy file
-			try{
-			do{
-			i=fin.read();
-			if(i!=-1) fout.write(i);
-			}while(i!=-1);
-			}
-			catch(IOException exc){
+		File f1=new File(args[0]);
+		FileWriter writer=new FileWriter(args[1]);
+		
+		//统计文件的有效行数（对应输出第三行）：任何包含非空白字符的行，都需要统计。
+		try{
+			int line=getLine(f1);
+			writer.write("lines："+line);
+			writer.close();
+			System.out.println("lines："+line);
+		}
+		catch(IOException exc){
 			System.out.println("File error!");
-			}
+		}
 
-			fin.close();
-			fout.close();
+	}
+	
+	public static int getLine(File file){
+		int line=0;
+		try {
+			if(file.exists()) {
+				long fileLength=file.length();
+				LineNumberReader lineNumberReader=new LineNumberReader(new FileReader(file));
+				lineNumberReader.skip(fileLength);
+				line=lineNumberReader.getLineNumber();
+				lineNumberReader.close();
 			}
+			else
+				System.out.println("file does not exist.");
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+		return line;
+	}
 }
