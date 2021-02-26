d126c4e9f254550ee66c6707f15ddfbd094923c3
miss-pud-in <79395007+miss-pud-in@users.noreply.github.com>
2021-02-26 17:19:21 +0800

    增加了统计单词数的功能
    1、新增统计单词数功能：除了统计单词数，countWords函数也将单词与其出现的频率存储在名为wordsMap的HashMap中——在实现统计词频功能时会用到wordsMap
    2、根据代码规范（附在codestyle.md文件中）修改部分函数的命名

--- 221801119/src/CoreCount.java
+++ 221801119/src/CoreCount.java
@@ -1,21 +1,30 @@
 package word.count.pro;
 
 import java.io.*;
+import java.util.*;
 import java.lang.String;
+import java.lang.Character;
 
+/**
+ * @author: mis-pud-in (221801119)
+ * @createDate: 2021/2/25
+ * @description: this includes core functions -- count characters, words and lines.
+ */
 public class CoreCount {
 	private File file;
 	private int charCount;
 	private int wordCount;
 	private int lineCount;
+	private Map<String, Integer> wordsMap;
 	public CoreCount(String fileName) {
 		this.file = new File(fileName);
 		charCount = wordCount = charCount = 0;
+		wordsMap = new HashMap<String, Integer>();
 	}
-	public void Count() {
-		CountChars();
-		CountWords();
-		CountLines();
+	public void count() {
+		countChars();
+		countWords();
+		countLines();
 	}
 	public int getCharCount() {
 		return charCount;
@@ -26,7 +35,7 @@ public class CoreCount {
 	public int getLineCount() {
 		return lineCount;
 	}
-	private void CountChars() {
+	private void countChars() {
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader(file));
 			try {
@@ -39,16 +48,52 @@ public class CoreCount {
 			System.out.print("Can not find file \"" + file.getName() + "\".");
 		}
 	}
-    private void CountWords() {
-		
+    private void countWords() {
+    	try {
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			try {
+				String line, word;
+				line = word = "";
+				char ch;
+				while((line = reader.readLine()) != null) {	
+					if(! line.trim().equals("")) {
+						line += "\n";
+						for(int i = 0; i < line.length(); i++) {
+							ch = line.charAt(i);
+							if(Character.isLetterOrDigit(ch))
+								word += ch;
+							else {
+								if(!"".equals(word)) {									
+									if(isProperWord(word)) {
+										word = word.toLowerCase();
+										System.out.print(word+" is a word.\n");
+										if(wordsMap.get(word) == null) {
+											wordCount += 1;
+											wordsMap.put(word, 1);
+										}
+										else {
+											int value = wordsMap.get(word);
+											wordsMap.put(word, ++value);
+										}
+									}
+									word = "";
+								}
+							}
+						}
+					}
+				}
+			} catch (IOException e) {
+			}
+		} catch (FileNotFoundException e) {
+			System.out.print("Can not find file \"" + file.getName() + "\".");
+		}	
 	}
-    private void CountLines() {
+    private void countLines() {
     	try {
 			BufferedReader reader = new BufferedReader(new FileReader(file));
 			try {
 				String line = null;
-				while((line = reader.readLine()) != null) {
-					//只统计包含非空白字符的行
+				while((line = reader.readLine()) != null) {	
 					if(! line.trim().equals(""))
 						lineCount += 1;
 				}
@@ -57,5 +102,16 @@ public class CoreCount {
 		} catch (FileNotFoundException e) {
 			System.out.print("Can not find file \"" + file.getName() + "\".");
 		}	
+    }
+    private boolean isProperWord(String word) {
+    	if(word.length() < 4) {
+    		return false;
+    	}
+    	for(int i = 0; i < 4; i++) {
+    		if(! Character.isLetter(word.charAt(i))) {
+    			return false;
+    		}
+    	}
+    	return true;
     }
 }
