330351e2df200eeebc84fb453beff4f5826e72e6
sujieyang <7768243+sujieyang@user.noreply.gitee.com>
2021-02-26 14:13:41 +0800

    工具类，实现所有功能

--- example/src/Utils/Util.java
+++ example/src/Utils/Util.java
@@ -8,7 +8,9 @@ import java.util.regex.Pattern;
  */
 public class Util {
 
-
+    /**
+     * 定义返回参数
+     */
     private static int word_num = 0;
     private static int char_num = 0;
 
@@ -32,5 +34,30 @@ public class Util {
         return char_num;
     }
 
-    
+    /**
+     * 根据正则表达式来统计单词数
+     *
+     * @param words
+     * @return
+     */
+    public static int wordNums(String words) {
+        // 构造正则表达式，去根据空格拆分整篇文章
+        // temp字符串数组将保存所有的单词
+        
+        String[] temp = words.split("\\s+");
+
+        // 根据题意
+        // 构造正则表达式筛选规则：以字母开头且长度大于4的单词
+        String regexs = "^[a-zA-Z]{4,}.*";
+
+        // 循环遍历这个数组，利用正则表达式去匹配
+        for (String i : temp) {
+            if (i.matches(regexs)) {
+                // 匹配成功，计数加1
+                word_num++;
+            }
+        }
+        return word_num;
+    }
+
 }
