66c668fc3c1fbe8d0faf47ea8366aad985c520a0
WWJ0720 <1920698385@qq.com>
2021-02-26 23:10:11 +0800

    update
    文件名由命令行输入

--- 221801326/src/WordCount.java
+++ 221801326/src/WordCount.java
@@ -3,9 +3,9 @@ import java.util.Map;
 
 public class WordCount {
     public static void main(String[] args) throws IOException {
-        int characters = Lib.charactersCount("C:\\Users\\WWJ20\\IdeaProjects\\WordCount\\src\\input.txt", "C:\\Users\\WWJ20\\IdeaProjects\\WordCount\\src\\output.txt");
-        int words = Lib.wordsCount("C:\\Users\\WWJ20\\IdeaProjects\\WordCount\\src\\input.txt", "C:\\Users\\WWJ20\\IdeaProjects\\WordCount\\src\\output.txt");
-        int lines = Lib.linesCount("C:\\Users\\WWJ20\\IdeaProjects\\WordCount\\src\\input.txt", "C:\\Users\\WWJ20\\IdeaProjects\\WordCount\\src\\output.txt");
-        Map<String, Integer> wordsMap = Lib.wordNum("C:\\Users\\WWJ20\\IdeaProjects\\WordCount\\src\\input.txt", "C:\\Users\\WWJ20\\IdeaProjects\\WordCount\\src\\output.txt");
+        int characters = Lib.charactersCount(args[0],args[1]);
+        int words = Lib.wordsCount(args[0],args[1]);
+        int lines = Lib.linesCount(args[0],args[1]);
+        Map<String, Integer> wordsMap = Lib.wordNum(args[0],args[1]);
     }
 }
