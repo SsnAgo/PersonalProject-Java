08b64e30ff019b7a170210b4d46d8f227e235d61
sujieyang <7768243+sujieyang@user.noreply.gitee.com>
2021-02-26 21:17:04 +0800

    生成测试文件

--- 221801424寒假2/src/Filetest.java
+++ 221801424寒假2/src/Filetest.java
@@ -4,16 +4,22 @@ public class Filetest {
 
     public static void main(String []args){
         Filetest test=new Filetest();
-        for (int i = 0; i < 1000000; i++) {
-            for (int j = 0; j < 10 ; j++){
+        for (int i = 0; i < 100000; i++) {
+            for (int j = 0; j < 5 ; j++){
                 test.stringBuilder.append("windows").append(i).append(" ");
             }
         }
         for (int i = 0; i < 10; i++) {
-            test.stringBuilder.append("sjy ");
+            test.stringBuilder.append("sjy1 ");
         }
         for (int i = 0; i < 10; i++) {
-            test.stringBuilder.append("sjydl12121, \n");
+            test.stringBuilder.append("sjydl12121,\n");
+        }
+        for (int i = 0; i < 2; i++) {
+            test.stringBuilder.append("wyc811,\n");
+        }
+        for (int i = 0; i < 6; i++) {
+            test.stringBuilder.append("wycsjy2099,\n");
         }
         test.message=test.stringBuilder.toString();
         Utils.writeTo("test.txt",test.message);
