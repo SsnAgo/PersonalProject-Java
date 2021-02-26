88aa804f1fefdaef26d069b2af0c69e808e5d508
sujieyang <7768243+sujieyang@user.noreply.gitee.com>
2021-02-26 14:34:57 +0800

    工具类，实现所有功能

--- example/src/Utils/Util.java
+++ example/src/Utils/Util.java
@@ -13,9 +13,9 @@ public class Util {
      */
     private static int word_num = 0;
     private static int char_num = 0;
-
+    private static int line_num = 0;
     /**
-     * 根据正则表达式来统计字符数
+     * 1.根据正则表达式来统计字符数
      *
      * @param character
      * @return
@@ -35,23 +35,21 @@ public class Util {
     }
 
     /**
-     * 根据正则表达式来统计单词数
-     *
+     * 2.根据正则表达式来统计单词数
      * @param words
      * @return
      */
     public static int wordNums(String words) {
         // 构造正则表达式，去根据空格拆分整篇文章
         // temp字符串数组将保存所有的单词
-        
-        String[] temp = words.split("\\s+");
 
-        // 根据题意
-        // 构造正则表达式筛选规则：以字母开头且长度大于4的单词
+        String[] temps = words.split("\\s+");
+
+        // 构造题意：以字母开头且长度大于4的单词
         String regexs = "^[a-zA-Z]{4,}.*";
 
         // 循环遍历这个数组，利用正则表达式去匹配
-        for (String i : temp) {
+        for (String i : temps) {
             if (i.matches(regexs)) {
                 // 匹配成功，计数加1
                 word_num++;
@@ -60,4 +58,29 @@ public class Util {
         return word_num;
     }
 
+
+    /**
+     * 3.根据整行读取实现获取行数
+     * @param path
+     * @return
+     */
+    public static int linesNumber(String path) {
+
+        try {
+            FileReader fileReader = new FileReader(path);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            // 通过循环不断整行读取文件
+            // 同时记录读取次数即可
+            while ((line = bufferedReader.readLine().strip(' ')) != null) {
+                //匹配任意非空白字符
+                if (line.length() != 0 && !line.matches("\\s+")) {
+                    line_num++;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return line_num;
+    }
 }
