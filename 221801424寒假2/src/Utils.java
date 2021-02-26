0927cbb82c320987da286f0a73e8c8aff8fa7235
sujieyang <7768243+sujieyang@user.noreply.gitee.com>
2021-02-26 19:53:01 +0800

    工具类,实现所有功能

--- 221801424寒假2/src/Utils.java
+++ 221801424寒假2/src/Utils.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -16,15 +14,18 @@ public class Utils {
      * 2.字符数
      * 3.行数
      * 4.key保存单词，value存储该单词出现的次数
+     * 5.存储文件读入信息
      */
     private static int word_num = 0;
     private static int char_num = 0;
     private static int line_num = 0;
-    private static HashMap<String,Integer> map = new HashMap<>();
+    private static HashMap<String, Integer> map = new HashMap<>();
+    private static StringBuilder stringBuilder = new StringBuilder();
 
 
     /**
      * 1.根据正则表达式来统计字符数
+     *
      * @param characters
      * @return
      */
@@ -45,6 +46,7 @@ public class Utils {
 
     /**
      * 2.根据正则表达式来统计单词数
+     *
      * @param words
      * @return
      */
@@ -71,6 +73,7 @@ public class Utils {
 
     /**
      * 3.根据整行读取实现获取行数
+     *
      * @param path
      * @return
      */
@@ -98,11 +101,12 @@ public class Utils {
 
     /**
      * 4.通过map来统计单词的出现次数
+     *
      * @param words
      * @return
      */
     @SuppressWarnings("JavaDoc")
-    public static List mapNums(String words){
+    public static List mapNums(String words) {
         String[] temps = words.split("\\s+");
         String regexs = "^[a-zA-Z]{4,}.*";
         for (String i : temps) {
@@ -112,7 +116,7 @@ public class Utils {
                     map.put(i.toLowerCase(), 1);
                 } else {
                     // 如果存在则num+1
-                    int num = (int)map.get(i.toLowerCase());
+                    int num = (int) map.get(i.toLowerCase());
                     map.put(i.toLowerCase(), num + 1);
                 }
             }
@@ -121,23 +125,68 @@ public class Utils {
         // 构造匿名内部类
         // 首先根据频率比较，如果频率相同，比较字典顺序
         list.sort((Comparator<Map.Entry>) (o1, o2)
-                -> ((Integer)o1.getValue()).compareTo((Integer) o2.getValue()) != 0
-                        ? ((Integer) o2.getValue()).compareTo((Integer) o1.getValue()) : ((String) o1.getKey()).compareTo((String) o2.getKey()));
+                -> ((Integer) o1.getValue()).compareTo((Integer) o2.getValue()) != 0
+                ? ((Integer) o2.getValue()).compareTo((Integer) o1.getValue()) : ((String) o1.getKey()).compareTo((String) o2.getKey()));
         // 返回list 前十个 数据，也即出现的前十的高频词
         return list.size() < 10 ? list.subList(0, list.size()) : list.subList(0, 10);
     }
 
-
     /**
-     * 测试
-     * @param args
+     * 用于读取文件所有字符
+     *
+     * @param path
+     * @return
      */
     @SuppressWarnings("JavaDoc")
-    public static void main(String[] args) {
-        //System.out.println(args[0]);
-        System.out.println(Utils.wordNums("ssss444 563ff 11d fase11 windows95 windows98 windows2000"));
-        System.out.println(Utils.charNums("ssss444 563ff 11d fase11 windows95 windows98 windows2000"));
-        System.out.println(Utils.mapNums("ssss444 563ff 11d fase11 windows95 windows98 windows2000"));
-        System.out.println(Utils.lineNums("E:\\寒假2\\221801424寒假2\\src\\1.txt"));
+    public static String readIn(String path) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(new File("").getAbsolutePath()+"\\"+path));
+            String temp;
+            while ((temp = bufferedReader.readLine()) != null) {
+                stringBuilder.append(temp).append("\n");
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println("文件不存在");
+            e.printStackTrace();
+        }
+        //处理特殊字符，以免被误以为是单词的一部分
+        String characters = stringBuilder.toString().replace(".", " ").replace(",", " ")
+                .replace("!", " ").replace("?", " ");
+        return characters;
     }
+
+
+    /**
+     * 写入信息
+     * @param path
+     * @param message
+     */
+    @SuppressWarnings("JavaDoc")
+    public static void writeTo(String path,String message) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(new File("").getAbsolutePath() + "\\" + path));
+            bw.write(message);
+            bw.flush();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    /**
+//     * 测试
+//     *
+//     * @param args
+//     */
+//    @SuppressWarnings("JavaDoc")
+//    public static void main(String[] args) {
+//        //System.out.println(args[0]);
+//        System.out.println(Utils.wordNums("ssss444 563ff 11d fase11 windows95 windows98 windows2000"));
+//        System.out.println(Utils.charNums("ssss444 563ff 11d fase11 windows95 windows98 windows2000"));
+//        System.out.println(Utils.mapNums("ssss444 563ff 11d fase11 windows95 windows98 windows2000"));
+//        System.out.println(Utils.lineNums(new File("").getAbsolutePath()+"\\1.txt"));
+//        System.out.println(new File("").getAbsolutePath());
+//        Utils.writeTo("2.txt","hello world");
+//    }
 }
