191e2c6505617de36e29eec3a8d9b96774b30700
destinylemon <60696580+destinylemon@users.noreply.github.com>
2021-02-27 20:57:34 +0800

    修改字符串截取出现的错误，增加打印词频功能

--- 221801304/src/Lib.java
+++ 221801304/src/Lib.java
@@ -24,8 +24,9 @@ public class Lib {
         int ch = 0;
         int countAlpha = 0;//字母数
         int wordLength = 3;//单词长度
+        boolean wordFlag = false;//是否成单词
         while((ch = fileReader.read()) != -1){
-            chars.append(ch);
+            chars.append((char) ch);
             if(isAlpha((char) ch))
                 countAlpha++;
             else{
@@ -33,14 +34,16 @@ public class Lib {
                     countAlpha = 0;//如果没有连续四个英文字母，计数清零
             }
             if(countAlpha >= 4){//如果有连续四个英文字母，合法单词诞生
+                wordFlag = true;
                 if(isAlpha((char) ch) || isNum((char) ch))
                     wordLength++;
                 else{//遇到分隔符，单词确定
+                    wordFlag = false;
                     int len = chars.length();
-                    String word = chars.substring(len - wordLength, len - 1).toLowerCase(Locale.ROOT);
+                    String word = chars.substring(len - wordLength - 1, len - 1).toLowerCase(Locale.ROOT);
                     if(map.containsKey(word)){
                         int value = map.get(word);
-                        map.put(word, value+1);
+                        map.put(word, value + 1);
                     }
                     else
                         map.put(word, 1);
@@ -48,7 +51,15 @@ public class Lib {
                     wordLength = 3;
                 }
             }
-
+        }
+        if(wordFlag){//防止读到结束时正在截取的单词的丢失
+            String word = chars.substring(chars.length() - wordLength, chars.length() - 1).toLowerCase(Locale.ROOT);
+            if(map.containsKey(word)){
+                int value = map.get(word);
+                map.put(word, value + 1);
+            }
+            else
+                map.put(word, 1);
         }
         fileReader.close();
         return map;
@@ -71,11 +82,32 @@ public class Lib {
 //
 //    }
 //
-//    public static void printWords(String inputFile, String outputFile) throws IOException{
-//        Map<String, Integer> map = handleWords(inputFile);
-//        FileWriter fileWriter = new FileWriter(outputFile);
-//
-//    }
+    public static void printWords(String inputFile, String outputFile) throws IOException{
+        Map<String, Integer> map = handleWords(inputFile);
+        FileWriter fileWriter = new FileWriter(outputFile, true);
+
+        //自定义比较器
+        Comparator<Map.Entry<String, Integer>> valCmp = new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o2.getValue() - o1.getValue();
+            }
+        };
+
+        //将map转成List，map的一组key，value对应list一个存储空间
+        List<Map.Entry<String, Integer>> list = new ArrayList<Map.Entry<String, Integer>>(map.entrySet());
+        Collections.sort(list, valCmp);
+
+        int size;//最高词频，最高为10个
+        if(list.size() < 10)
+            size = list.size();
+        else
+            size = 10;
+        for(int i = 0; i < size; i++) {
+            fileWriter.write("\n" + list.get(i).getKey() + ": " + list.get(i).getValue());
+        }
+        fileWriter.close();
+    }
 
     public static boolean isAlpha(char ch){
         if(((ch >= 'a') && (ch <= 'z')) || ((ch >= 'A') && (ch <= 'Z')))
@@ -89,11 +121,5 @@ public class Lib {
             return true;
         else
             return false;
-    }
-
-    public static char[] subBytes(char[] src, int begin, int count) {
-        char[] bs = new char[count];
-        System.arraycopy(src, begin, bs, 0, count);
-        return bs;
     }
 }
--- 221801304/src/WordCount.java
+++ 221801304/src/WordCount.java
@@ -5,5 +5,6 @@ public class WordCount {
     public static void main(String[] args) throws IOException {
         int count = Lib.countChars("221801304/src/input.txt","221801304/src/output.txt");
         int num = Lib.countWords("221801304/src/input.txt","221801304/src/output.txt");
+        Lib.printWords("221801304/src/input.txt","221801304/src/output.txt");
     }
 }
