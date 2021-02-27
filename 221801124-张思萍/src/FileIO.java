781761eee5b6376a3d909e93c79181dc37dd06d1
fzu-zsp <934110724@qq.com>
2021-02-27 13:44:11 +0800

    add readFile
    Co-Authored-By: fzu-zsp <79618598+fzu-zsp@users.noreply.github.com>

--- 221801124-张思萍/src/FileIO.java
+++ 221801124-张思萍/src/FileIO.java
@@ -11,5 +11,34 @@ public class FileIO {
         return reader;
     }
 
+    public static ArrayList<String> readFile(String filePath){
+        File file=new File(filePath);
+        if(!file.exists()){
+            return null;
+        }
+        BufferedReader reader=null;
+        ArrayList<String> strs=new ArrayList<>();
+        try{
+            reader = new BufferedReader(new FileReader(filePath));
+            String temp;
+            while ((temp=reader.readLine())!=null){
+                strs.add(temp);
+            }
+            reader.close();
+            return strs;
+        }catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }finally {
+            if(reader!=null){
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 
 }
