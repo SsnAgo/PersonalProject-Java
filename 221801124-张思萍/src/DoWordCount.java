876f0b47b00992df86db45169cf895d58a91a730
fzu-zsp <934110724@qq.com>
2021-02-27 16:00:33 +0800

    add printTop10
    Co-Authored-By: fzu-zsp <79618598+fzu-zsp@users.noreply.github.com>

--- 221801124-张思萍/src/DoWordCount.java
+++ 221801124-张思萍/src/DoWordCount.java
@@ -7,26 +7,28 @@ public class DoWordCount {
 
     //统计字符数，空格，水平制表符，换行符，均算字符
     public static int countChars(String filePath) throws IOException {
-        Reader reader=FileIO.getReader(filePath);
-        int sum=0;
+        Reader reader = FileIO.getReader(filePath);
+        int sum = 0;
         int tempChar;
-        while ((tempChar=reader.read())!=-1){
+
+        while ((tempChar = reader.read()) != -1) {
             sum++;
         }
         return sum;
     }
 
     //统计文件的单词总数 单词：至少以4个英文字母开头，跟上字母数字符号，单词以分隔符分割，不区分大小写。
-    public static int countWords(ArrayList<String> lines) throws IOException{
-        int sum=0;
+    public static int countWords(ArrayList<String> lines) throws IOException {
+        int sum = 0;
         String line;
         String[] words;
-        for (int i=0;i<lines.size();i++){
-            line=lines.get(i);
-            words=line.split("[^a-zA-Z0-9]+");
-            for (int j=0;j<words.length;j++){
-                char[] word=words[j].toCharArray();
-                if(isValidWord(word)){
+
+        for (int i = 0; i < lines.size(); i++) {
+            line = lines.get(i);
+            words = line.split("[^a-zA-Z0-9]+");
+            for (int j = 0; j < words.length; j++) {
+                char[] word = words[j].toCharArray();
+                if (isValidWord(word)) {
                     sum++;
                 }
             }
@@ -35,13 +37,14 @@ public class DoWordCount {
     }
 
     //统计文件的有效行数：任何包含非空白字符的行，都需要统计。
-    public static int countLines(ArrayList<String> lines) throws IOException{
-        int sum=0;
+    public static int countLines(ArrayList<String> lines) throws IOException {
+        int sum = 0;
         String line;
+
         for (int i = 0; i < lines.size(); i++) {
-            line=lines.get(i);
-            line=line.replaceAll("\\s*","");
-            if(!(line.equals(""))){
+            line = lines.get(i);
+            line = line.replaceAll("\\s*", "");
+            if (!(line.equals(""))) {
                 sum++;
             }
         }
@@ -54,6 +57,7 @@ public class DoWordCount {
         String line;
         String[] words;
         String tempWord;
+
         for (int i = 0; i < lines.size(); i++) {
             line = lines.get(i);
             words = line.split("[^a-zA-Z0-9]+");
@@ -85,21 +89,33 @@ public class DoWordCount {
         return mappingList;
     }
 
+    //输出使用频率前10的单词
+    public static String printTop10(List<Map.Entry<String, Integer>> maplist) {
+        int cnt = 0;
+        String outToFile = "";
+
+        for (int i = 0; i < maplist.size(); i++) {
+            Map.Entry<String, Integer> e = maplist.get(i);
+            outToFile += e.getKey() + ": " + e.getValue() + "\n";
+            if (cnt++ >= 9) break;
+        }
+        return outToFile;
+    }
+
     //是否为有效单词
-    public static boolean isValidWord(char[] word){
-        if(word.length>=4&&isAlpha(word[0])&&isAlpha(word[1])&&isAlpha(word[2])&&isAlpha(word[3])){
+    public static boolean isValidWord(char[] word) {
+        if (word.length >= 4 && isAlpha(word[0]) && isAlpha(word[1]) && isAlpha(word[2]) && isAlpha(word[3])) {
             return true;
         }
         return false;
     }
 
     //是否为字母
-    public static boolean isAlpha(int temp){
+    public static boolean isAlpha(int temp) {
         if ((temp >= 97 && temp <= 122) || (temp >= 65 && temp <= 90)) {
             return true;
         } else {
             return false;
         }
     }
-
 }
