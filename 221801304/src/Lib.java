2b8c8da048bee8f66d341ad9945ec1a9deebf100
destinylemon <60696580+destinylemon@users.noreply.github.com>
2021-02-27 20:00:09 +0800

    优化字符的处理与map填充

--- 221801304/src/Lib.java
+++ 221801304/src/Lib.java
@@ -1,16 +1,12 @@
-import com.sun.deploy.util.ArrayUtil;
 
 import java.io.*;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Lib {
 
-    public static int countChar(String inputFile,String outputFile) throws IOException {
+    public static int countChars(String inputFile, String outputFile) throws IOException {
         FileReader fileReader = new FileReader(inputFile);
-        FileWriter fileWriter = new FileWriter(outputFile,true);
+        FileWriter fileWriter = new FileWriter(outputFile, true);
         int characters = 0;//文件中的字符数
         while(fileReader.read() != -1){
             characters++;
@@ -21,49 +17,65 @@ public class Lib {
         return characters;
     }
 
-    public static int countWord(String inputFile,String outputFile) throws IOException{
+    public static Map<String, Integer> handleWords(String inputFile) throws IOException{
         FileReader fileReader = new FileReader(inputFile);
-        FileWriter fileWriter = new FileWriter(outputFile,true);
         Map<String, Integer> map = new HashMap<>();
-        char[] chars = new char[1024];//缓冲区数组
-        int len;//字符数组大小
-        while(fileReader.read(chars) != -1){
-            len = chars.length;
-            int countAlpha = 0;//字母数
-            int wordLength = 3;//单词长度
-            for(int i = 0; i < len; i++){
-                if(isAlpha(chars[i]))
-                    //如果是英文字母，就+1
-                    countAlpha++;
-                else{
-                    if(countAlpha < 4)
-                        //如果没有连续四个英文字母，计数清零
-                        countAlpha = 0;
-                }
-                if(countAlpha >= 4){//如果有连续四个英文字母，合法单词诞生
-                    if(isAlpha(chars[i]) || isNum(chars[i]))
-                        wordLength++;
-                    else{//遇到分隔符，单词确定
-                        char[] ch = subBytes(chars,i - wordLength,wordLength);
-                        String word = ch.toString();
-                        if(map.containsKey(word)){
-                            int value = map.get(word);
-                            map.put(word,value+1);
-                        }
-                        else
-                            map.put(word,1);
-                        countAlpha = 0;
-                        wordLength = 3;
+        StringBuffer chars = new StringBuffer();
+        int ch = 0;
+        int countAlpha = 0;//字母数
+        int wordLength = 3;//单词长度
+        while((ch = fileReader.read()) != -1){
+            chars.append(ch);
+            if(isAlpha((char) ch))
+                countAlpha++;
+            else{
+                if(countAlpha < 4)
+                    countAlpha = 0;//如果没有连续四个英文字母，计数清零
+            }
+            if(countAlpha >= 4){//如果有连续四个英文字母，合法单词诞生
+                if(isAlpha((char) ch) || isNum((char) ch))
+                    wordLength++;
+                else{//遇到分隔符，单词确定
+                    int len = chars.length();
+                    String word = chars.substring(len - wordLength, len - 1).toLowerCase(Locale.ROOT);
+                    if(map.containsKey(word)){
+                        int value = map.get(word);
+                        map.put(word, value+1);
                     }
+                    else
+                        map.put(word, 1);
+                    countAlpha = 0;
+                    wordLength = 3;
                 }
             }
+
         }
-        int words = map.size();
-        fileWriter.write("words: "+ words);
         fileReader.close();
+        return map;
+    }
+
+    public static int countWords(String inputFile, String outputFile) throws IOException{
+        Map<String, Integer> map = handleWords(inputFile);
+        FileWriter fileWriter = new FileWriter(outputFile, true);
+        int words = map.size();
+        fileWriter.write("\n");
+        fileWriter.write("words: " + words);
         fileWriter.close();
         return words;
     }
+
+//    public static int countLines(String inputFile, String outputFile) throws IOException{
+//        FileReader fileReader = new FileReader(inputFile);
+//        FileWriter fileWriter = new FileWriter(outputFile, true);
+//        int lines = 0;
+//
+//    }
+//
+//    public static void printWords(String inputFile, String outputFile) throws IOException{
+//        Map<String, Integer> map = handleWords(inputFile);
+//        FileWriter fileWriter = new FileWriter(outputFile);
+//
+//    }
 
     public static boolean isAlpha(char ch){
         if(((ch >= 'a') && (ch <= 'z')) || ((ch >= 'A') && (ch <= 'Z')))
@@ -74,13 +86,6 @@ public class Lib {
 
     public static boolean isNum(char ch){
         if(ch >= '0' && ch <= '9')
-            return true;
-        else
-            return false;
-    }
-
-    public static boolean isSpace(char ch){
-        if(ch == ' ')
             return true;
         else
             return false;
--- 221801304/src/WordCount.java
+++ 221801304/src/WordCount.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class WordCount {
 
     public static void main(String[] args) throws IOException {
-        int count = Lib.countChar("221801304/src/input.txt","221801304/src/output.txt");
-        int num = Lib.countWord("221801304/src/input.txt","221801304/src/output.txt");
+        int count = Lib.countChars("221801304/src/input.txt","221801304/src/output.txt");
+        int num = Lib.countWords("221801304/src/input.txt","221801304/src/output.txt");
     }
 }
