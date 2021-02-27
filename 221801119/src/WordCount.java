93c2705bee96b1a6952259cab096506a45f51c71
miss-pud-in <79395007+miss-pud-in@users.noreply.github.com>
2021-02-27 20:46:43 +0800

    增加了将输出内容写入文件的功能
    依次调用CoreCount的get函数读取字符数、单词数、行数和词频，并将读取到的数据按一定格式写入到用户指定的输出文件中。

--- 221801119/src/WordCount.java
+++ 221801119/src/WordCount.java
@@ -1,32 +1,57 @@
 package word.count.pro;
 
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
+
 import java.lang.String;
-    
+
+/**
+ * @author: mis-pud-in (221801119)
+ * @createDate: 2021/2/25
+ * @description: this includes the main function.
+ */
 public class WordCount {
 	public static void main(String[] args) {
-		//读取文件名
+		//get filename from users
 		Scanner input = new Scanner(System.in);
 		String inputStr = input.nextLine();
 		input.close();
         String[] files = inputStr.split(" "); 
         String inputFile = files[0];
         String outputFile = files[1];
-		//通过封装核心功能的接口计算
+		//main function
         CoreCount coreCount = new CoreCount(inputFile);
-        coreCount.Count();
-        /* 测试用：
-        System.out.print(coreCount.getCharCount());
-        */
-        //生成输出文件
+        coreCount.count();
+        /*
+        System.out.print("characters: " + coreCount.getCharCount() + "\n");
+        System.out.print("words: " + coreCount.getWordCount() + "\n");
+        System.out.print("lines: " + coreCount.getLineCount() + "\n");
+        Iterator<Map.Entry<String,Integer>> iterator = coreCount.getWordsList().iterator();
+        while (iterator.hasNext()) {
+        	Map.Entry<String,Integer> entry = (Map.Entry<String,Integer>) iterator.next();
+        	System.out.print(entry.getKey().toString() + ": " + entry.getValue().toString() + "\n");
+        }*/
+        //create output file
 		File file = new File(outputFile);
 		if (file.exists()) {
 			System.out.print("File \"" + outputFile + "\" already exist.");
 		}
 		try {
 			file.createNewFile();
-		} catch (IOException e){			
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+			writer.append("characters: " + coreCount.getCharCount() + "\n");
+			writer.append("words: " + coreCount.getWordCount() + "\n");
+			writer.append("lines: " + coreCount.getLineCount() + "\n");
+			int wordsNum = 0;
+			Iterator<Map.Entry<String,Integer>> iterator = coreCount.getWordsList().iterator();
+	        while (iterator.hasNext() && wordsNum < 10) {
+	        	Map.Entry<String,Integer> entry = (Map.Entry<String,Integer>) iterator.next();
+	        	writer.append(entry.getKey().toString() + ": " + entry.getValue().toString() + "\n");
+	        	wordsNum += 1;
+	        }
+	        writer.close();
+		} catch (IOException e) {
 		}
-    }
+	} 
 }
+
