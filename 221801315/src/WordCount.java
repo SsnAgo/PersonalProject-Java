04bdffbaac6bf18c5de0df297b6216a6c2459189
Aurora <784536133@qq.com>
2021-02-28 16:10:03 +0800

    check the files'path are read correctly and write .gitignore

---
+++ 221801315/src/WordCount.java
@@ -0,0 +1,9 @@
+public class WordCount {
+    public static void main(String[] args) {
+        String infile=args[0];
+        String outfile=args[1];
+
+        System.out.println(infile);
+        System.out.println(outfile);
+    }
+}
