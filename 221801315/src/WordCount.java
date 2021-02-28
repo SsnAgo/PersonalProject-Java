cce0eca9f4d0b576a1082fd0c04bf4e825bed243
Aurora <784536133@qq.com>
2021-02-28 18:51:47 +0800

    add my code style

--- 221801315/src/WordCount.java
+++ 221801315/src/WordCount.java
@@ -1,3 +1,11 @@
+/**
+ * 功能：统计用户搜索记录
+ * 作者：张福荣
+ * 学号：221801315
+ * 邮箱：784536133@qq.com
+ * 创建时间：2021/2/28 15:20
+ * 最后修改时间：2021/2/28 18:00
+ */
 public class WordCount {
     public static void main(String[] args) {
         String infile=args[0];
