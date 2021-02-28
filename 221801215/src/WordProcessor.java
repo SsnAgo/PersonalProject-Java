c6fa2713cdf36602a2c94049cbef457b08cbb498
Tars <SETars@outlook.com>
2021-02-28 15:12:05 +0800

    - 实现从文件中抽离单词功能  * 为WordProcessor添加函数，将文件中的字符组成仅含字母和数字的字符串

--- 221801215/src/WordProcessor.java
+++ 221801215/src/WordProcessor.java
@@ -2,6 +2,15 @@
  * 对单词校验、统计的类
  */
 public class WordProcessor {
+    private boolean isInPossibleWord = false;
+    private final int INIT_WORD_MAX_LENGTH=200;
+    private boolean isPossibleWordAvailable;
+    /**
+     * The Possible word.
+     */
+    public StringBuffer possibleWord;
+
+
     /**
      * 判断字符串是否符合单词要求
      *
@@ -21,13 +30,13 @@ public class WordProcessor {
         if(wordLength>3) {
             int i = 0;
             for ( ; i < 4; i++) {
-                if (!(Character.isLetter(stringToBeExam.charAt(i)))) {
+                if (!(isLetterChar(stringToBeExam.charAt(i)))) {
                     isLegal = false;
                     break;
                 }
             }
             for ( ; i < wordLength && isLegal; i++) {
-                if ((!Character.isLetter(stringToBeExam.charAt(i))) && (!Character.isDigit(stringToBeExam.charAt(i)))) {
+                if ((!isLetterChar(stringToBeExam.charAt(i))) && (!isDigitChar(stringToBeExam.charAt(i)))) {
                     isLegal = false;
                     break;
                 }
@@ -36,5 +45,41 @@ public class WordProcessor {
             isLegal = false;
         }
         return isLegal;
+    }
+
+    /**
+     * 将读入的字符组成可能的单词
+     *
+     * <p>可能的单词为仅由字母和数字组成的字符串</p>
+     *
+     * @param scanChar    the scan char
+     * @param isEndOfFile the is end of file
+     * @return the boolean
+     */
+    public boolean buildPossibleWord(char scanChar,boolean isEndOfFile) {
+        isPossibleWordAvailable = false;
+        if (!isEndOfFile) {
+            if (isLetterChar(scanChar) || isDigitChar(scanChar)) {
+                if (!isInPossibleWord) {
+                    isInPossibleWord = true;
+                    possibleWord = new StringBuffer(INIT_WORD_MAX_LENGTH);
+                }
+                possibleWord.append(scanChar);
+            } else {
+                if (isInPossibleWord) {
+                    isInPossibleWord = false;
+                    isPossibleWordAvailable = true;
+                }
+            }
+        }else {
+            isPossibleWordAvailable = isInPossibleWord;
+        }
+        return isPossibleWordAvailable;
+    }
+    private boolean isLetterChar(char charToExam) {
+        return (charToExam >= 'a' && charToExam <='z') || (charToExam >= 'A' && charToExam <= 'Z');
+    }
+    private boolean isDigitChar(char charToExam) {
+        return (charToExam >= '0' && charToExam <= '9');
     }
 }
