3f4ebef732d239e535404036ab56eeaeb62b849c
Aurora <784536133@qq.com>
2021-02-28 21:18:48 +0800

    add exception handling when files don't exit

--- 221801315/src/Lib.java
+++ 221801315/src/Lib.java
@@ -1,10 +1,35 @@
+import java.io.*;
+
 /**
  * 功能：WordCount中main方法要调用的函数
  * 作者：张福荣
  * 学号：221801315
  * 邮箱：784536133@qq.com
  * 创建时间：2021/2/28 15:22
- * 最后修改时间：2021/2/28 18:00
+ * 最后修改时间：2021/2/28 20:21
  */
 public class Lib {
+    /* 检测所给文件路径是否有效，输入文件不存在则抛出异常，输出文件不存在则创建
+       输入参数：输入文件路径inFilePath，输出文件路径outFilePath
+       返回值：空
+       异常：输入文件不存在异常FileNotFoundException*/
+    public static void checkFileValid(String inFilePath, String outFilePath) throws FileNotFoundException{
+        File inFile = new File(inFilePath);
+        //当输入文件不存在时，打印提示信息，抛出异常
+        if (!inFile.exists()) {
+            System.out.println("File " + inFilePath + " doesn't exist, program will exit.");
+            throw new FileNotFoundException();
+        }
+
+        File outFile = new File(outFilePath);
+        //当输出文件不存在时，创建文件
+        if (!outFile.exists()) {
+            try {
+                outFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
 }
--- 221801315/src/WordCount.java
+++ 221801315/src/WordCount.java
@@ -1,17 +1,27 @@
+import java.io.FileNotFoundException;
+
 /**
  * 功能：统计用户搜索记录
  * 作者：张福荣
  * 学号：221801315
  * 邮箱：784536133@qq.com
  * 创建时间：2021/2/28 15:20
- * 最后修改时间：2021/2/28 18:00
+ * 最后修改时间：2021/2/28 20:21
  */
 public class WordCount {
     public static void main(String[] args) {
-        String infile=args[0];
-        String outfile=args[1];
+        //获取文件路径，没有输入文件路径和输出文件路径则报错，并退出程序
+        if (args.length != 2) {
+            System.out.println("Please enter 2 files' path!!!");
+            System.exit(-1);
+        }
+        String inFilePath = args[0];
+        String outFilePath = args[1];
 
-        System.out.println(infile);
-        System.out.println(outfile);
+        try {
+            Lib.checkFileValid(inFilePath,outFilePath);
+        } catch (FileNotFoundException e) {
+            System.exit(-1);
+        }
     }
 }
