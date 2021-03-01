85aa153edc81a24d50ddca7ef74a9a79d4942ea2
destinylemon <60696580+destinylemon@users.noreply.github.com>
2021-03-01 10:06:34 +0800

    进一步封装函数，提高复用性，让每个功能尽可能分开

--- 221801304/src/Lib.java
+++ 221801304/src/Lib.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class Lib {
 
+    //统计字符数
     public static int countChars(String inputFile, String outputFile) throws IOException {
         FileReader fileReader = new FileReader(inputFile);
         FileWriter fileWriter = new FileWriter(outputFile, true);
@@ -17,11 +18,12 @@ public class Lib {
         return characters;
     }
 
+    //统计单词并填充入map
     public static Map<String, Integer> handleWords(String inputFile) throws IOException{
         FileReader fileReader = new FileReader(inputFile);
         Map<String, Integer> map = new HashMap<>();
         StringBuffer chars = new StringBuffer();
-        int ch = 0;
+        int ch;//每次读取到的字符
         int countAlpha = 0;//字母数
         int wordLength = 3;//单词长度
         boolean wordFlag = false;//是否成单词
@@ -33,40 +35,40 @@ public class Lib {
                 if(countAlpha < 4)
                     countAlpha = 0;//如果没有连续四个英文字母，计数清零
             }
-            if(countAlpha >= 4){//如果有连续四个英文字母，单词诞生
-                int len = chars.length();
+            if(countAlpha >= 4){//有连续四个英文字母
                 wordFlag = true;
+                int len = chars.length();
                 if(isAlpha((char) ch) || isNum((char) ch))
                     wordLength++;
-                else{//遇到分隔符，单词确定
+                else{//遇到分隔符
                     wordFlag = false;
-                    String word = chars.substring(len - wordLength - 1, len - 1).toLowerCase(Locale.ROOT);
-                    if(word.length() < len - 1 && isNum(chars.charAt(len - wordLength - 2))){
-                        //单词前有分隔符或无字符才算是单词
-                    }else if(map.containsKey(word)){
-                        int value = map.get(word);
-                        map.put(word, value + 1);
-                    }else
-                        map.put(word, 1);
+                    insertMap(map, chars, wordLength, len);
                     countAlpha = 0;
                     wordLength = 3;
                 }
             }
         }
         if(wordFlag){//防止读到结束时正在截取的单词的丢失
-            int len = chars.length();
-            String word = chars.substring(len - wordLength, len).toLowerCase(Locale.ROOT);
-            if(word.length() < len - 1 && isNum(chars.charAt(len - wordLength - 1))){
-            }else if(map.containsKey(word)){
-                int value = map.get(word);
-                map.put(word, value + 1);
-            }else
-                map.put(word, 1);
+            int len = chars.length() + 1;
+            insertMap(map, chars, wordLength, len);
         }
         fileReader.close();
         return map;
     }
 
+    //判断单词前是否为分隔符或者空格（因为要复用所以提取出来），是则填充map
+    public static void insertMap(Map<String, Integer> map, StringBuffer chars, int wordLength, int len){
+        String word = chars.substring(len - wordLength - 1, len - 1).toLowerCase(Locale.ROOT);
+        if(word.length() < len - 1 && isNum(chars.charAt(len - wordLength - 2))){
+            //单词前有分隔符或无字符才算是单词
+        }else if(map.containsKey(word)){
+            int value = map.get(word);
+            map.put(word, value + 1);
+        }else
+            map.put(word, 1);
+    }
+
+    //从map提取数据计算并打印单词数
     public static int countWords(String inputFile, String outputFile) throws IOException{
         Map<String, Integer> map = handleWords(inputFile);
         FileWriter fileWriter = new FileWriter(outputFile, true);
@@ -80,6 +82,7 @@ public class Lib {
         return words;
     }
 
+    //统计有效行数
     public static int countLines(String inputFile, String outputFile) throws IOException{
         FileReader fileReader = new FileReader(inputFile);
         FileWriter fileWriter = new FileWriter(outputFile, true);
@@ -91,9 +94,7 @@ public class Lib {
                 int len = str.length();
                 for (int i = 0; i < len; i++) {
                     char ch = str.charAt(i);
-                    if (ch == '\n' || ch == '\t' || ch == '\r' || ch == 32) {
-                        continue;
-                    } else {
+                    if (!isBlank(ch)) {
                         lines++;
                         break;
                     }
@@ -109,44 +110,37 @@ public class Lib {
         return lines;
     }
 
+    //从map提取词频最多的十个单词并打印
     public static void printWords(String inputFile, String outputFile) throws IOException{
         Map<String, Integer> map = handleWords(inputFile);
         FileWriter fileWriter = new FileWriter(outputFile, true);
 
         //自定义比较器
-        Comparator<Map.Entry<String, Integer>> valCmp = new Comparator<Map.Entry<String, Integer>>() {
-            @Override
-            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                return o2.getValue() - o1.getValue();
-            }
-        };
+        Comparator<Map.Entry<String, Integer>> valCmp = (o1, o2) -> o2.getValue() - o1.getValue();
 
         //将map转成List，map的一组key，value对应list一个存储空间
-        List<Map.Entry<String, Integer>> list = new ArrayList<Map.Entry<String, Integer>>(map.entrySet());
-        Collections.sort(list, valCmp);
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort(valCmp);
 
-        int size;//最高词频，最高为10个
-        if(list.size() < 10)
-            size = list.size();
-        else
-            size = 10;
+        int size = Math.min(list.size(), 10);//最高词频，最高为10个
         for(int i = 0; i < size; i++) {
             fileWriter.write("\n" + list.get(i).getKey() + ": " + list.get(i).getValue());
         }
         fileWriter.close();
     }
 
+    //判断是否是字母
     public static boolean isAlpha(char ch){
-        if(((ch >= 'a') && (ch <= 'z')) || ((ch >= 'A') && (ch <= 'Z')))
-            return true;
-        else
-            return false;
+        return ((ch >= 'a') && (ch <= 'z')) || ((ch >= 'A') && (ch <= 'Z'));
     }
 
+    //判断是否是数字
     public static boolean isNum(char ch){
-        if(ch >= '0' && ch <= '9')
-            return true;
-        else
-            return false;
+        return ch >= '0' && ch <= '9';
+    }
+
+    //判断是否是空白符
+    public static boolean isBlank(char ch){
+        return ch == '\n' || ch == '\t' || ch == '\r' || ch == 32;
     }
 }
