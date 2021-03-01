9885a9040db6904a83190732d68671ef22b286f3
QAQWMZ <79703668+QAQWMZ@users.noreply.github.com>
2021-03-01 18:35:28 +0800

    Add writeFile()

--- 021800623/src/Lib.java
+++ 021800623/src/Lib.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Lib {
 
@@ -6,10 +8,18 @@ public class Lib {
     private String writeFileName;
     private BufferedReader bufferedReader;
     private BufferedWriter bufferedWriter;
+    private int charNum;
+    private int wordNum;
+    private int lineNum;
+    private Map<String,Integer> wordMap;
 
     public Lib(String readFileName, String writeFileName) {
         this.readFileName = readFileName;
         this.writeFileName = writeFileName;
+        charNum = 0;
+        wordNum = 0;
+        lineNum = 0;
+        wordMap = new HashMap<String, Integer>();
     }
 
    private String readFile() throws IOException {
@@ -21,10 +31,28 @@ public class Lib {
            {
                fileBuilder.append((char)c);
            }
-
        } catch (FileNotFoundException e) {
            e.printStackTrace();
        }
        return fileBuilder.toString();
+   }
+
+   public void writeFile() throws IOException {
+       String fileContent = readFile();
+       charNum = fileContent.length();
+       bufferedWriter = new BufferedWriter(new FileWriter(writeFileName));
+       bufferedWriter.write("characters: "+charNum+"\n");
+       bufferedWriter.write("words: "+wordNum+"\n");
+       bufferedWriter.write("lines: "+lineNum+"\n");
+       int i = 1;
+       for (Map.Entry<String,Integer> entry:wordMap.entrySet())
+       {
+           if (i>10) {
+               break;
+           }
+           bufferedWriter.write(entry.getKey()+": "+entry.getValue()+"\n");
+           i++;
+       }
+       bufferedWriter.flush();
    }
 }
