7a652ee086592b20df7118ede0f19a3e0572e00b
KQ-one <1055275112@qq.com>
2021-03-01 22:27:42 +0800

    实现读取文件内容

--- 221801120/src/Lib.java
+++ 221801120/src/Lib.java
@@ -1,2 +1,60 @@
+import java.io.*;
+
 public class Lib {
+    private String inputFile;
+    private String outputFile;
+
+    /**
+     * 构造函数
+     * @param inputFile 读取文件地址
+     * @param outputFile 写入文件地址
+     */
+    public Lib(String inputFile, String outputFile) {
+        this.inputFile = inputFile;
+        this.outputFile = outputFile;
+    }
+
+    /**
+     * 读取文件内容
+     */
+    public String readFile() throws IOException {
+        BufferedReader reader = null;
+        StringBuilder builder = new StringBuilder();
+        try{
+            reader = new BufferedReader(new FileReader(inputFile));
+            int num = 0;
+            char ch;
+            while ((num = reader.read()) != -1) {
+                ch = (char) num;
+                builder.append(ch);
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        finally {
+            if (reader != null)
+                reader.close();
+        }
+        return builder.toString();
+    }
+
+    /**
+     * 写入文件
+     */
+    public void writeFile() throws IOException {
+        String content = readFile();
+        BufferedWriter writer = null;
+        try{
+            writer = new BufferedWriter(new FileWriter(outputFile));
+            writer.write(content);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        finally {
+            if (writer != null)
+                writer.close();
+        }
+    }
 }
--- 221801120/src/WordCount.java
+++ 221801120/src/WordCount.java
@@ -1,2 +1,16 @@
+import java.io.IOException;
+
 public class WordCount {
+    public static void main(String[] args) throws IOException {
+        if (args.length < 2){
+            System.out.println("Insufficient parameters");
+        }
+        else{
+            String inputFile = args[0];
+            String outputFile = args[1];
+            Lib lib = new Lib(inputFile, outputFile);
+            lib.writeFile();
+            System.out.println("succeed");
+        }
+    }
 }
