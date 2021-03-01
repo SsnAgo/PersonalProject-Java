9ff939886ebfbfe88506ae9fd6053ec0f7c472e4
Aurora <784536133@qq.com>
2021-03-01 15:05:14 +0800

    add function counting total words

--- 221801315/src/Lib.java
+++ 221801315/src/Lib.java
@@ -6,7 +6,7 @@ import java.io.*;
  * 学号：221801315
  * 邮箱：784536133@qq.com
  * 创建时间：2021/2/28 15:22
- * 最后修改时间：2021/2/28 20:21
+ * 最后修改时间：2021/3/1 15:04
  */
 public class Lib {
     /* 检测所给文件路径是否有效，输入文件不存在则抛出异常，输出文件不存在则创建
@@ -37,11 +37,12 @@ public class Lib {
        返回值：字符总数count */
     public static int countTotalChar(Reader in) {
         int count = 0;    //记录字符总数
+
         try {
             int temp = in.read();  //记录读取的字符
             //读取文件，直到文件结束
             while (temp != -1) {
-                if (temp >= 0 && temp <= 127)
+                if (temp >= 0 && temp <= 127)   //题目要求只需统计ASCII码
                     ++count;
                 temp = in.read();
             }
@@ -50,5 +51,72 @@ public class Lib {
         }
 
         return count;
+    }
+
+    /* 统计输入文件中的单词总数
+       输入参数：文件读取流in
+       返回值：单词总数count */
+    public static int countTotalWord(Reader in) {
+        int count = 0;    //记录单词总数
+        int letterCount = 0;     //字母数
+
+        try {
+            int temp = in.read();  //记录读取的字符
+            //读取文件，直到文件结束
+            while (temp != -1) {
+                if (isLetter((char) temp))
+                    ++letterCount;
+                else {
+                    //是单词，则继续读取直到分隔符
+                    if (letterCount >= 4) {
+                        while (!isSeparator((char) temp)) {
+                            temp = in.read();
+                        }
+                        ++count;
+                        letterCount = 0;
+                    } else {
+                        //发现不是单词后，直接跳到分隔符，开始判断下一个单词
+                        while (!isSeparator((char) temp)) {
+                            temp = in.read();
+                        }
+                        letterCount = 0;
+                    }
+                }
+                temp = in.read();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return count;
+    }
+
+    /* 判断字符是否是字母
+       输入参数：字符t
+       返回值：判断结果的bool值 */
+    public static boolean isLetter(char t) {
+        if ((t >= 'a' && t <= 'z') || (t >= 'A' && t <= 'Z'))
+            return true;
+        return false;
+    }
+
+    /* 判断字符是否是数字
+       输入参数：字符t
+       返回值：判断结果的bool值 */
+    public static boolean isDigit(char t) {
+        if (t >= '0' && t <= '9')
+            return true;
+        return false;
+    }
+
+    /* 判断字符是否是分隔符
+       输入参数：字符t
+       返回值：判断结果的bool值 */
+    public static boolean isSeparator(char t) {
+        if (isLetter(t))
+            return false;
+        else if (isDigit(t))
+            return false;
+        return true;
     }
 }
