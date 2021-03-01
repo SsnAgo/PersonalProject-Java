aab1577d6a80d287ac5af0b688c29de6ea88c5de
imcjx <850839565@qq.com>
2021-03-01 17:40:36 +0800

    Count the number of characters and lines

--- 221801113/src/WordCount.java
+++ 221801113/src/WordCount.java
@@ -1,7 +1,37 @@
+import java.io.*;
+
 public class WordCount {
 
+    public static Reader readFile(String fileName) throws IOException {
+        return new BufferedReader(new FileReader(fileName));
+    }
 
-    public static void main(String[] args) {
-        System.out.println("hello");
+    public static int charactersCount(String fileName) throws IOException {
+        Reader reader = readFile(fileName);
+        int ch = 0, cnt = 0;
+        while ((ch = reader.read()) != -1) {
+            cnt++;
+            //0-127?
+        }
+        reader.close();
+        return cnt;
+    }
+
+    public static int linesCount(String fileName) throws IOException {
+        BufferedReader reader = (BufferedReader) readFile(fileName);
+        int cnt = 0;
+        String curLine = null;
+        while ((curLine = reader.readLine()) != null) {
+            if (!curLine.replaceAll("\r|\n", "").trim().equals("")) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) throws IOException {
+        String test = "input.txt";
+        System.out.println(charactersCount(test));
+        System.out.println(linesCount(test));
     }
 }
