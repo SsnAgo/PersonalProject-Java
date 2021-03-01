07ed4959832b857f964dff49b539758bda49f0b4
Tars <SETars@outlook.com>
2021-03-02 00:08:47 +0800

    - 实现统计各个单词独立的出现频数功能  * 统计单词各自出现的次数，单词不区分大小写

--- 221801215/src/WordProcessor.java
+++ 221801215/src/WordProcessor.java
@@ -1,15 +1,23 @@
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * 对单词校验、统计的类
  */
 public class WordProcessor {
     private boolean isInPossibleWord = false;
-    private final int INIT_WORD_MAX_LENGTH=200;
+    private final int INIT_WORD_MAX_LENGTH = 200;
     private boolean isPossibleWordAvailable;
+    private Map<String,Integer> wordSumMap = new TreeMap<>();
+
     /**
      * The Possible word.
      */
     public StringBuffer possibleWord;
-
+    /**
+     * The Word sum.
+     */
+    public int wordSum = 0;
 
     /**
      * 判断字符串是否符合单词要求
@@ -21,22 +29,22 @@ public class WordProcessor {
      * eg.
      * file123是一个单词， 123file不是一个单词。</p>
      *
-     * @param stringToBeExam the string 需要检测的字符串
+     * @param stringBufferToBeExam the stringBuffer 需要检测的字符串
      * @return the boolean 字符串检测的结果
      */
-    public boolean isLegalWord(String stringToBeExam) {
+    public boolean isLegalWord(StringBuffer stringBufferToBeExam) {
         boolean isLegal = true;
-        int wordLength = stringToBeExam.length();
+        int wordLength = stringBufferToBeExam.length();
         if(wordLength>3) {
             int i = 0;
             for ( ; i < 4; i++) {
-                if (!(isLetterChar(stringToBeExam.charAt(i)))) {
+                if (!(isLetterChar(stringBufferToBeExam.charAt(i)))) {
                     isLegal = false;
                     break;
                 }
             }
             for ( ; i < wordLength && isLegal; i++) {
-                if ((!isLetterChar(stringToBeExam.charAt(i))) && (!isDigitChar(stringToBeExam.charAt(i)))) {
+                if ((!isLetterChar(stringBufferToBeExam.charAt(i))) && (!isDigitChar(stringBufferToBeExam.charAt(i)))) {
                     isLegal = false;
                     break;
                 }
@@ -76,6 +84,26 @@ public class WordProcessor {
         }
         return isPossibleWordAvailable;
     }
+
+    /**
+     * 统计各个单词独立的出现频数（不区分大小写）
+     *
+     * <p>按照传入的单词统计各自出现的次数，返回该传入的单词是否已统计过<em>单词不区分大小写</em></p>
+     *
+     * @param legalWord the legal 要统计的合法单词
+     * @return the boolean 传入的单词是否已统计过
+     */
+    public boolean individualWordSumUp(String legalWord) {
+        boolean isCounted = false;
+        if (wordSumMap.containsKey(legalWord)) {
+            wordSumMap.replace(legalWord,wordSumMap.get(legalWord) + 1);
+            isCounted = true;
+        }else {
+            wordSumMap.put(legalWord,1);
+        }
+        return isCounted;
+    }
+
     private boolean isLetterChar(char charToExam) {
         return (charToExam >= 'a' && charToExam <='z') || (charToExam >= 'A' && charToExam <= 'Z');
     }
