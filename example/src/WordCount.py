a738d1378229961be5ccffde931c7e681ff03913
mayrain-yh <1183336841@qq.com>
2021-03-02 16:41:15 +0800

    修改相应函数 新增输出文件函数

--- example/src/WordCount.py
+++ example/src/WordCount.py
@@ -2,14 +2,18 @@
 import re
 import copy
 
+#统计文件的字符数（对应输出第一行）
 def count_chars(file_name):
     c_cs = 0
     with open(file_name) as file_obj:
         for content in file_obj:
             c_cs += count_line_chars(content)
-    print("characters：" + str(c_cs))
+    m = "characters：" + str(c_cs)+"\n"
+    # print(m)
+    out_file("output.txt",m)
 
-#统计文件的字符数（对应输出第一行）
+
+#计算文件中每一行的字符数
 def count_line_chars(content):
     result = 0
     for i in set(content):
@@ -23,13 +27,14 @@ def count_word(file_name):
     r = re.compile(r"[,!\*\.]")
     with open(file_name , "r") as f:
         for line in f:
-            for word in r.sub("", line.strip()).split(" "):
+            for word in r.sub("", line.strip()).lower().split(" "):
                 if word in words:
                     words[word] += 1
                 words.setdefault(word , 1)
     result = sum(words.values())
     #print(words)
-    print("words："+str(result))
+    m = "words："+str(result)+"\n"
+    out_file("output.txt",m)
 
 
 #统计文件的有效行数
@@ -41,7 +46,8 @@ def count_lines(file_name):
         if len(line) == 0:
             continue
         s += 1
-    print("lines："+str(s))
+    m = "lines："+str(s)+"\n"
+    out_file("output.txt",m)
 
 
 #统计文件中的各单词出现次数
@@ -50,7 +56,7 @@ def count_words(file_name):
     r = re.compile(r"[,!?\*\.]")
     with open(file_name, "r") as f:
         for line in f:
-            for word in r.sub("", line.strip()).split(" "):
+            for word in r.sub("", line.strip()).lower().split(" "):
                 if word in words:
                     words[word] += 1
                 words.setdefault(word, 1)
@@ -63,13 +69,28 @@ def count_words(file_name):
     m = 0
     for i in words2:
         if (m<10):
-            print(str(i[0])+":"+str(i[1]))
+            s = str(i[0])+":"+str(i[1])+"\n"
+            out_file("output.txt", s)
             m+=1
 
+def out_file(filename,s):
+    filename = 'output.txt'
+    with open(filename, 'a') as file_object:
+        file_object.write(s)
+
+def clear_file(filename):
+    with open(filename, 'w') as f1:
+        f1.seek(0)
+        f1.truncate()
+    # f1.close()
 
 if __name__ == '__main__':
-    file = "input.txt"
-    count_chars(file)
-    count_word(file)
-    count_lines(file)
-    count_words(file)
+
+    file1 = input("请输入文件路径：")
+    # file2 = input("请输入输出文件名称：")
+    # file = str
+    clear_file("output.txt")
+    count_chars(file1)
+    count_word(file1)
+    count_lines(file1)
+    count_words(file1)
