2453f5d989019f19d6ca0f03296041cbe3c66ac1
kepachirenguai <79777840+kepachirenguai@users.noreply.github.com>
2021-03-02 21:07:30 +0800

    Update WordCount.java
    加入了计算有几个字符修改了编码方式

--- 221801418/src/WordCount.java
+++ 221801418/src/WordCount.java
@@ -9,36 +9,34 @@ public class WordCount {
         try {
                 File file=new File(filePath);
                 if(file.isFile() && file.exists()){
-                    InputStreamReader read = new InputStreamReader(new FileInputStream(file),"GBK");
+                    InputStreamReader read = new InputStreamReader(new FileInputStream(file),"UTF-8");
                     BufferedReader bufferedReader = new BufferedReader(read);
                     StringBuilder lineText = new StringBuilder();
                     int lines=0;
                     String temp;
                     while((temp=bufferedReader.readLine()) !=null){
-                    	lineText.append(temp);
-                        System.out.println(lineText);
+                    	lineText.append(temp+"\n");
                         lines++;
                     }      
                     read.close();
-                    //countLetters(lineText.toString());
+                    System.out.print(countChars(lineText.toString()));
                     //countTimes();
-                    //countNumbers(lineText);
+                    //countWords(lineText);
                     
                 }else{
-                	System.out.println("找不到指定的文件");
+                	System.out.println("找不到该文档！");
                 }
         } catch (Exception e) {
-            System.out.println("读取文件内容出错");
+            System.out.println("打开文档出错！");
             e.printStackTrace();
         }
      
     }
-    /*public static void countLetters(String str) {
-    	
-        
-    }*/
+    public static int countChars(String str) {
+    	return str.length();        
+    }
     public static void main(String argv[]){
-        readTxtFile("src\\input.txt");
+        readTxtFile("input.txt");
     }
      
 }
