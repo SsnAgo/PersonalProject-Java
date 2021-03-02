e6f2c1dcaa36bc59cb2ca8e920ede33996dbee44
imcjx <850839565@qq.com>
2021-03-02 12:27:15 +0800

    将读文件抽离为一个函数，只读一次不用读三次

--- 221801113/src/Lib.java
+++ 221801113/src/Lib.java
@@ -1,3 +1,133 @@
+import java.io.*;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Lib {
+
+    public static int countCharacters(String str) {
+        return str.length();
+    }
+
+    public static int countLines(String str) {
+        int cnt = 0;
+        String[] strLine = str.split("\r\n|\n");
+        for (String validLine : strLine) {
+            if (!validLine.replaceAll("\r|\n", "").trim().equals("")) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public static int countWords(String str, HashMap<String, Integer> wordMap) {
+        int cnt = 0;
+        Pattern pattern = Pattern.compile("^[a-z]{4}[a-z0-9]*");
+        Matcher matcher = null;
+        String[] wordStrTmp = str.split("[^a-zA-Z0-9]");
+        for (String word : wordStrTmp) {
+            word = word.toLowerCase();
+            matcher = pattern.matcher(word);
+            if (!word.equals("") && matcher.find()) {
+                cnt++;
+                if(wordMap.containsKey(word)) {
+                    wordMap.put(word, wordMap.get(word) + 1);
+                }
+                else {
+                    wordMap.put(word, 1);
+                }
+            }
+        }
+        return cnt;
+    }
+
+    public static List<HashMap.Entry<String, Integer>> getSortedList(HashMap<String, Integer> wordMap) {
+        List<HashMap.Entry<String, Integer>> list =
+                new ArrayList<HashMap.Entry<String, Integer>>(wordMap.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<String,Integer>>() {
+            public int compare(Map.Entry<String,Integer> o1, Map.Entry<String,Integer> o2){
+                if(o1.getValue().equals(o2.getValue()))
+                    return o1.getKey().compareTo(o2.getKey());
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
+        return list;
+    }
+
+
+
+
+/*public static Reader readFile(String fileName) throws IOException {
+        return new BufferedReader(new FileReader(fileName));
+    }
+
+    public static void countCharacters(String fileName) throws IOException {
+        Reader reader = readFile(fileName);
+        int ch = 0, cnt = 0;
+        while ((ch = reader.read()) != -1) {
+            cnt++;
+        }
+        reader.close();
+        charsNum = cnt;
+    }
+
+    public static void countLines(String fileName) throws IOException {
+        BufferedReader reader = (BufferedReader) readFile(fileName);
+        int cnt = 0;
+        String curLine = null;
+        while ((curLine = reader.readLine()) != null) {
+            if (!curLine.replaceAll("\r|\n", "").trim().equals("")) {
+                cnt++;
+            }
+        }
+        reader.close();
+        linesNum = cnt;
+    }
+
+    public static void countWords(String fileName) throws IOException {
+        BufferedReader reader = (BufferedReader) readFile(fileName);
+        hashMap = new HashMap<String, Integer>();
+        int cnt = 0;
+        Pattern pattern = Pattern.compile("^[a-z]{4}[a-z0-9]*");
+        Matcher matcher = null;
+        String curLine = null;
+        while ((curLine = reader.readLine()) != null) {
+            String[] wordStrTmp = curLine.split("[^a-zA-Z0-9]");
+            for (String word : wordStrTmp) {
+                word = word.toLowerCase();
+                matcher = pattern.matcher(word);
+                if (!word.equals("") && matcher.find()) {
+                    cnt++;
+                    if(hashMap.containsKey(word)) {
+                        hashMap.put(word, hashMap.get(word) + 1);
+                    }
+                    else {
+                        hashMap.put(word, 1);
+                    }
+                }
+            }
+        }
+        wordsNum = cnt;
+    }
+
+
+
+    public static void writeToFile(String inputPath, String OutputPath) throws IOException {
+        countCharacters(inputPath);
+        countLines(inputPath);
+        countWords(inputPath);
+        BufferedWriter writer = new BufferedWriter(new FileWriter(OutputPath));
+        int cnt = 0;
+        StringBuilder str = new StringBuilder("characters: " + charsNum + '\n' + "words: " + wordsNum + '\n'
+                + "lines: " + linesNum + '\n');
+        List<HashMap.Entry<String, Integer>> sortedList = getSortedList();
+        for(HashMap.Entry<String,Integer> entry:sortedList) {
+            str.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
+            cnt++;
+            if (cnt >= 10) break;
+        }
+        writer.write(str.toString());
+        writer.close();
+    }*/
 
 }
--- 221801113/src/WordCount.java
+++ 221801113/src/WordCount.java
@@ -4,96 +4,62 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WordCount {
-    private static HashMap<String, Integer> hashMap;
+    private static HashMap<String, Integer> wordMap;
     private static int charsNum;
     private static int linesNum;
     private static int wordsNum;
+    private static String textStr;
 
-    public static Reader readFile(String fileName) throws IOException {
-        return new BufferedReader(new FileReader(fileName));
+    public WordCount(String fileName) throws IOException {
+        wordMap = new HashMap<>();
+        textStr = readFile(fileName);
+        charsNum = Lib.countCharacters(textStr);
+        linesNum = Lib.countLines(textStr);
+        wordsNum = Lib.countWords(textStr, wordMap);
     }
 
-    public static int charactersCount(String fileName) throws IOException {
-        Reader reader = readFile(fileName);
-        int ch = 0, cnt = 0;
-        while ((ch = reader.read()) != -1) {
-            cnt++;
+    public String readFile(String fileName) throws IOException {
+        BufferedReader reader = null;
+        StringBuilder str = new StringBuilder();
+        int ch = 0;
+        try {
+            reader = new BufferedReader(new FileReader(fileName));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-        reader.close();
-        return cnt;
-    }
-
-    public static int linesCount(String fileName) throws IOException {
-        BufferedReader reader = (BufferedReader) readFile(fileName);
-        int cnt = 0;
-        String curLine = null;
-        while ((curLine = reader.readLine()) != null) {
-            if (!curLine.replaceAll("\r|\n", "").trim().equals("")) {
-                cnt++;
+        try {
+            while ((ch = reader.read()) != -1) {
+                str.append((char)ch);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            reader.close();
         }
-        reader.close();
-        return cnt;
+        return str.toString();
     }
 
-    public static int wordsCount(String fileName) throws IOException {
-        BufferedReader reader = (BufferedReader) readFile(fileName);
-        hashMap = new HashMap<String, Integer>();
-        int cnt = 0;
-        Pattern pattern = Pattern.compile("^[a-z]{4}[a-z0-9]*");
-        Matcher matcher = null;
-        String curLine = null;
-        while ((curLine = reader.readLine()) != null) {
-            String[] wordStrTmp = curLine.split("[^a-zA-Z0-9]");
-            for (String word : wordStrTmp) {
-                word = word.toLowerCase();
-                matcher = pattern.matcher(word);
-                if (!word.equals("") && matcher.find()) {
-                    cnt++;
-                    if(hashMap.containsKey(word)) {
-                        hashMap.put(word, hashMap.get(word) + 1);
-                    }
-                    else {
-                        hashMap.put(word, 1);
-                    }
-                }
-            }
-        }
-        return cnt;
-    }
-
-    public static List<HashMap.Entry<String, Integer>> getSortedList() {
-        List<HashMap.Entry<String, Integer>> list = new ArrayList<HashMap.Entry<String, Integer>>(hashMap.entrySet());
-        Collections.sort(list, new Comparator<Map.Entry<String,Integer>>() {
-            public int compare(Map.Entry<String,Integer> o1, Map.Entry<String,Integer> o2){
-                if(o1.getValue().equals(o2.getValue()))
-                    return o1.getKey().compareTo(o2.getKey());
-                return o2.getValue().compareTo(o1.getValue());
-            }
-        });
-        return list;
-    }
-
-    public static void writeToFile(String path) throws IOException {
-        BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+    public static void writeFile(String OutputPath) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(OutputPath));
         int cnt = 0;
         StringBuilder str = new StringBuilder("characters: " + charsNum + '\n' + "words: " + wordsNum + '\n'
-            + "lines: " + linesNum + '\n');
-        List<HashMap.Entry<String, Integer>> sortedList = getSortedList();
+                + "lines: " + linesNum + '\n');
+        List<HashMap.Entry<String, Integer>> sortedList = Lib.getSortedList(wordMap);
         for(HashMap.Entry<String,Integer> entry:sortedList) {
             str.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
             cnt++;
             if (cnt >= 10) break;
         }
-        System.out.println(str.toString());
+        writer.write(str.toString());
+        writer.close();
     }
 
-
     public static void main(String[] args) throws IOException {
-        String test = "input.txt";
-        charsNum = charactersCount(test);
-        linesNum = linesCount(test);
-        wordsNum = wordsCount(test);
-        writeToFile("output.txt");
+        if (args.length != 2){
+            System.out.println("命令行参数个数错误");
+            return;
+        }
+        WordCount wordCount = new WordCount(args[0]);
+        writeFile(args[1]);
     }
 }
