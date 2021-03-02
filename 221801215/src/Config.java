9801f84e560ee850c34d46539c524e8693538670
Tars <SETars@outlook.com>
2021-03-03 01:09:02 +0800

    - WordCount核心模块封装  *添加WordCountCore类提供统计字符数、单词数、最多的10个单词及其词频功能

--- 221801215/src/Config.java
+++ 221801215/src/Config.java
@@ -1,4 +1,8 @@
 public class Config {
     public static final char NEW_LINE_CHAR = '\n';
     public static final String CHARSET="UTF-8";
+
+    public static final String FILE_NOT_FOUND_EXCEPTION_TIP = "未找到文件，请检查文件路径或文件名";
+    public static final String UNSUPPORTED_ENCODING_EXCEPTION_TIP = "不支持的编码格式，请使用" + CHARSET + "编码的文件";
+    public static final String IO_EXCEPTION_TIP = "文件读写出错，请稍后重试";
 }
