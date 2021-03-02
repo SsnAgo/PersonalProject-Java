248c368043a6983208a03ee2f8f516c1d7b012e4
cqsg <137402865@qq.com>
2021-03-02 15:28:31 +0800

    Update WordCount.java

--- example/src/WordCount.java
+++ example/src/WordCount.java
@@ -1,3 +1,4 @@
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -8,25 +9,26 @@ public class WordCount {
     public static void main(String args[]) 
     { 
       	 String finname = args[0];
-      	 
+      	 //String foutname = args[1];
     } 
     public void wordmanage(String finname) {
-    	
     	try 
         { 
-    		FileInputStream fr=new FileInputStream(new File(finname)); 
-
-         int i=0; 
-         while((i=fr.read())!=-1) 
-         { 
-          System.out.print((char)i); 
-         } 
-         fr.close(); 
+    		 FileInputStream fr=new FileInputStream(new File(finname)); 
+	         int i=0; 
+	         int wordcount=0;
+	         int canbeword=0;
+	         while((i=fr.read())!=-1) 
+	         {
+	        	 
+	        	 
+	         } 
+	         fr.close(); 
         } 
         catch(ArrayIndexOutOfBoundsException ex) 
         { 
          System.out.println("\nPlease Enter the File Name in Command Line Argument. \n"+ 
-         "For Example :- java MyFileReader C:\\Users\\Desktop\\File1.txt"); 
+         "For Example :- java WordCount File1.txt File2.txt"); 
         } 
         catch(IOException ex) 
         { 
