348c640dd4261b19980035e2d72e7c8a218f2956
LiarX <58362731+LiarX@users.noreply.github.com>
2021-03-02 20:54:24 +0800

    第三次commit
    完成统计文件的单词总数功能

--- 221801409/src/Lib.java
+++ 221801409/src/Lib.java
@@ -13,6 +13,7 @@ public class Lib {
             while((str=br.readLine())!=null){
                 result.append(str+"\n");
             }
+            br.close();
         }catch (FileNotFoundException e){
             e.printStackTrace();
         } catch (Exception e){
@@ -26,6 +27,23 @@ public class Lib {
         for(int i=0;i<ch.length;i++){
             if(ch[i]>=0&&ch[i]<128){
                 count++;
+            }
+            //只计算ACSII字符
+        }
+        return count;
+    }
+    public static int getWordsCount(String str){
+        int count=0;
+        String temp=null;
+        String regex="^[a-z]+$";
+        str=str.toLowerCase();//将字符串变成小写
+        String[] strArray=str.split("[^a-z0-9]+");
+        //用split方法将非字母数字的字符作为分割线分割成单词数组，字符串已经是小写了，不存在A-Z
+        for(int i=0;i<strArray.length;i++){
+            if(strArray[i].length()>=4){//至少要四个英文字母开头，也就是说单词至少需要四个字符
+                temp=strArray[i].substring(0,3);//取出分割后字符串的前四个字符
+                if(temp.matches(regex))
+                    count++;
             }
         }
         return count;
--- 221801409/src/WordCount.java
+++ 221801409/src/WordCount.java
@@ -2,5 +2,6 @@ public class WordCount {
     public static void main(String[] args) {
         System.out.println(Lib.readFormTxt("D:/123.txt"));
         System.out.println(Lib.getCharactersCount(Lib.readFormTxt("D:/123.txt")));
+        System.out.println(Lib.getWordsCount(Lib.readFormTxt("D:/123.txt")));
     }
 }
