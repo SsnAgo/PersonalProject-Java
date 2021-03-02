7ee292103051c7fd1d6a21a4a61a44adefce8190
LiarX <58362731+LiarX@users.noreply.github.com>
2021-03-02 17:49:53 +0800

    第二次commit
    完成字符数统计功能

--- 221801409/src/Lib.java
+++ 221801409/src/Lib.java
@@ -9,9 +9,9 @@ public class Lib {
         StringBuilder result=new StringBuilder();
         try{
             BufferedReader br=new BufferedReader(new FileReader(file));
-            String temp=null;
-            while((temp=br.readLine())!=null){
-                result.append(temp+"\n");
+            String str=null;
+            while((str=br.readLine())!=null){
+                result.append(str+"\n");
             }
         }catch (FileNotFoundException e){
             e.printStackTrace();
@@ -19,5 +19,15 @@ public class Lib {
             e.printStackTrace();
         }
         return result.toString();
+    }
+    public static int getCharactersCount(String str){
+        int count=0;
+        char[] ch=str.toCharArray();
+        for(int i=0;i<ch.length;i++){
+            if(ch[i]>=0&&ch[i]<128){
+                count++;
+            }
+        }
+        return count;
     }
 }
--- 221801409/src/WordCount.java
+++ 221801409/src/WordCount.java
@@ -1,5 +1,6 @@
 public class WordCount {
     public static void main(String[] args) {
         System.out.println(Lib.readFormTxt("D:/123.txt"));
+        System.out.println(Lib.getCharactersCount(Lib.readFormTxt("D:/123.txt")));
     }
 }
