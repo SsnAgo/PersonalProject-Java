2fefdb061fd98b4010566ba9c76e4a9b759908ed
kepachirenguai <79777840+kepachirenguai@users.noreply.github.com>
2021-03-02 22:39:58 +0800

    Update WordCount.java
    完善了一下检测单词数量的功能，稍微加了一些注释

--- 221801418/src/WordCount.java
+++ 221801418/src/WordCount.java
@@ -2,24 +2,25 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WordCount {
 
-	public static void readTxtFile(String filePath){
+	public static void readTextFile(String filePath){
         try {
                 File file=new File(filePath);
                 if(file.isFile() && file.exists()){
                     InputStreamReader read = new InputStreamReader(new FileInputStream(file),"UTF-8");
                     BufferedReader bufferedReader = new BufferedReader(read);
                     StringBuilder lineText = new StringBuilder();
-                    int lines=0;
+                    int lines=0;//用于统计行数
                     String temp;
                     while((temp=bufferedReader.readLine()) !=null){
                     	lineText.append(temp+"\n");
                         lines++;
                     }      
-                    read.close();
-                    
+                    read.close();        
                     System.out.println("characters:"+countChars(lineText.toString()));
                     System.out.println("words:"+countWords(lineText.toString()));
                     System.out.print("lines:"+lines);
@@ -34,19 +35,25 @@ public class WordCount {
         }
      
     }
-    public static int countChars(String str) {
-    	return str.length();        
+    public static int countChars(String str) {	//返回字符数量
+    	return str.length()-1;        
     }
-    public static int countWords(String str) {
+    public static int countWords(String str) {	//返回单词数量
     	str=str.toLowerCase();
-    	String[] strArray=str.split("[^a-z0-9]");
+    	String[] strArray=str.split("[^a-z0-9]");	//先以除了数字字母的字符来分割
     	int words=0;
+    	List<String> list = new ArrayList<String>();
+    	for(int i=0;i<strArray.length; i++) {
+    	   if (!list.contains(strArray[i])) {
+    	       list.add(strArray[i]);
+    	   }
+    	}
+    	strArray = list.toArray(new String[list.size()]);//把字符串数组过滤为没有重复元素的
     	for(int i=0;i<strArray.length;i++) {
     		if(strArray[i].length()<4)continue;
     		else {
-    			System.out.println(strArray[i]);
-    			String temp=strArray[i].substring(1,4);
-    			if(!temp.matches("[0-9]")) {
+    			String temp=strArray[i].substring(0,4);
+    			if(temp.matches("[a-z]*")) {		//判断是否符合单词规则
     				words++;
     			}
     		}
@@ -54,7 +61,7 @@ public class WordCount {
     	return words;
     }
     public static void main(String argv[]){
-        readTxtFile("input.txt");
+        readTextFile("input.txt");
     }
      
 }
