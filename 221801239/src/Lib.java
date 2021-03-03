3477c179f1d30fb1daa8f8e6966969cd8ea83eb6
robot-gif <57623968+robot-gif@users.noreply.github.com>
2021-03-04 00:38:47 +0800

    Update Lib.java
    编写打开文件的函数

--- 221801239/src/Lib.java
+++ 221801239/src/Lib.java
@@ -1,1 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Lib {
+
+    public static BufferedReader openInputFile(String fileName) {
+        BufferedReader bufferedReader = null;
+
+        try {
+            bufferedReader = new BufferedReader(new FileReader(fileName));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return bufferedReader;
+    }
+}
 
