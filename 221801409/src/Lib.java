6b7a759f9d46b711b394770deeaa394980d72e42
LiarX <58362731+LiarX@users.noreply.github.com>
2021-03-04 02:22:38 +0800

    第十次commit
    修复了行数统计问题，进行了重新排版，程序基本完成

--- 221801409/src/Lib.java
+++ 221801409/src/Lib.java
@@ -1,5 +1,3 @@
-
-
 import java.io.*;
 import java.util.*;
 
@@ -10,7 +8,7 @@ public class Lib {
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
             int temp;
-            while ((temp = br.read()) !=-1) {
+            while ((temp = br.read()) != -1) {
                 result.append((char) temp);
             }
             br.close();
@@ -52,13 +50,19 @@ public class Lib {
     }
 
     public static int getLineCount(String str) {
-        str = str.replaceAll("[^\\S\\r\\n]", "");//将str中除了换行符以外的空白字符删掉
-        String count[] =str.split("\r|\n|\r\n");//将新字符串以split方法进行换行符分割后，利用length方法统计行数
-        return count.length;
+        List<String> lineList = new ArrayList<String>();
+        str = str.replaceAll("[ |\r|\t]+", "");//将str中除了换行符以外的空白字符删掉
+        String count[] = str.split("\n+");//将新字符串以split方法进行换行符分割，作为行数统计基准
+        for (int i = 0; i < count.length; i++) {//由于后来发现如果两个换行符连续出现，则会split会将空字符串一并计入，因此用另一个表只记录非空值的部分
+            if (count[i] != "") lineList.add(count[i]);
+        }
+        for (int i = 0; i < count.length; i++)
+            System.out.println(count[i]);
+        return lineList.size();
     }
 
     public static List getWordFrequency(String str) {
-        HashMap <String,Integer>map = new HashMap<String, Integer>();
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
         String temp = null;
         String regex = "^[a-z]+$";
         str = str.toLowerCase();//将字符串变成小写
@@ -78,31 +82,31 @@ public class Lib {
         Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {//修改比较器达到排序目的
             @Override
             public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                if(o2.getValue().compareTo(o1.getValue())==0){//同值情况按字典排序
+                if (o2.getValue().compareTo(o1.getValue()) == 0) {//同值情况按字典排序
                     return o1.getKey().compareTo(o2.getKey());
-                }
-                else
+                } else
                     return o2.getValue().compareTo(o1.getValue());//不同值情况按值排序
             }
         });
         return list;
     }
-    public static int writeToTxt(String txtPath,String str){
-        File file=new File(txtPath);
-        List<Map.Entry<String, Integer>> list=Lib.getWordFrequency(str);
+
+    public static int writeToTxt(String txtPath, String str) {
+        File file = new File(txtPath);
+        List<Map.Entry<String, Integer>> list = Lib.getWordFrequency(str);
         try {
-            BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file),"utf-8"));
-            bw.write("Characters: "+Lib.getCharactersCount(str)+"\n");
-            bw.write("words: "+Lib.getWordsCount(str)+"\n");
-            bw.write("lines: "+Lib.getLineCount(str)+"\n");
-            for(int i=0;i<(list.size()<10?list.size():10);i++){
-                bw.write(list.get(i).getKey()+": "+list.get(i).getValue()+"\n");
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "utf-8"));
+            bw.write("Characters: " + Lib.getCharactersCount(str) + "\n");
+            bw.write("words: " + Lib.getWordsCount(str) + "\n");
+            bw.write("lines: " + Lib.getLineCount(str) + "\n");
+            for (int i = 0; i < (list.size() < 10 ? list.size() : 10); i++) {
+                bw.write(list.get(i).getKey() + ": " + list.get(i).getValue() + "\n");
             }
             bw.flush();
             bw.close();
-        }catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return 0;
--- 221801409/src/WordCount.java
+++ 221801409/src/WordCount.java
@@ -1,6 +1,6 @@
 public class WordCount {
     public static void main(String[] args) {
-        String str=Lib.readFormTxt(args[0]);
-        Lib.writeToTxt(args[1],str);
+        String str = Lib.readFormTxt(args[0]);
+        Lib.writeToTxt(args[1], str);
     }
 }
