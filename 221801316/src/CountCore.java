87970f552f5c0e83f60952d29763ccc0c2d70e21
Liangdayi <1113901523@qq.com>
2021-03-04 00:09:25 +0800

    单词统计功能

--- 221801316/src/CountCore.java
+++ 221801316/src/CountCore.java
@@ -1,4 +1,8 @@
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 
 public class CountCore {
     public static int characterCount(File inputFile){
@@ -37,8 +41,40 @@ public class CountCore {
         }catch (IOException e){
             e.printStackTrace();
         }
-        
-
-
     }
+    public static boolean isWord(String string){
+        String wordRegex="[a-z]{4}[a-z0-9]*";
+        return Pattern.matches(wordRegex,string);
+    }
+    public static Map<String,Integer> wordCount(File inputFile){
+        Map<String,Integer> result=new HashMap<>();
+        BufferedReader bufferedReader=null;
+        StringBuilder stringBuilder=new StringBuilder();
+        String splitRegex="[^a-zA-Z0-9]";
+        try {
+            String str;
+            bufferedReader=new BufferedReader(new FileReader(inputFile));
+            while((str=bufferedReader.readLine())!=null){
+                str=str.toLowerCase();
+                //将分隔符全部替换成" "
+                str=str.replaceAll(splitRegex," ");
+                StringTokenizer stringTokenizer=new StringTokenizer(str);
+                while(stringTokenizer.hasMoreTokens()){
+                    String substring=stringTokenizer.nextToken();
+                    if(isWord(substring)&&result.containsKey(substring)){
+                        int frequency=result.get(substring);
+                        result.put(substring,frequency+1);
+                    }else if(isWord(substring)&&(!result.containsKey(substring))){
+                        result.put(substring,1);
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    
 }
