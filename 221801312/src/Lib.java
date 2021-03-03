8b18c2f5ca9bf67be398b4e519236a28e7511574
Savona <905899183@qq.com>
2021-03-03 21:25:02 +0800

    优化了性能和编写了测试函数

--- 221801312/src/Lib.java
+++ 221801312/src/Lib.java
@@ -55,45 +55,17 @@ public class Lib {
         return wordNum;
     }
 
-    //统计每行的单词数，返回long
-    private static long countLineWordsNum(String line){
-        long wordsNum=0;
-        int wordFlag=0;
-        StringBuilder tempWord=new StringBuilder();     //存单词
-        for(int i=0;i<line.length();i++){
-            if(Character.isLetter(line.charAt(i))){
-                wordFlag++;                             //如果是字母，将wordFlag++;
-                tempWord.append(line.charAt(i));        //用于清空tempWord
-            }
-            else if(Character.isDigit(line.charAt(i)) && wordFlag>=4){
-                wordFlag++;                             //当wordFlag>=4时，判定为一个单词，若后续为数字，将wordFlag继续++
-                tempWord.append(line.charAt(i));
-            }
-            else if(wordFlag<4){                        //当wordFlag<4且遇到字符为数字时，说明不可能为单词，将wordFlag置零
-                wordFlag=0;
-                tempWord.delete(0,tempWord.length());
-            }
-            if(!Character.isLetterOrDigit(line.charAt(i)) || i==line.length()-1){
-                if(wordFlag>=4){                            //当遇到分隔符或检索到行尾时
-                    wordsNum++;                            //若有wordFlag>=4，判定为单词有效，将有效单词++
-                    String tempWordString=tempWord.toString().toLowerCase();
-                }
-                wordFlag=0;                                 //不论是否为有效单词，都将wordFlag置零
-                tempWord.delete(0,tempWord.length());
-            }
-        }
-        return wordsNum;
-    }
 
     public static Map<String, Long> countWordFrequency(BufferedReader tempReader) throws IOException {
         return sortWord(countWordsTable(tempReader,false));
     }
 
+    static StringBuilder tempWord=new StringBuilder();
     //统计单词，返回map
     private static Hashtable<String,Long> countLineWords(String line,Hashtable<String,Long> mapWord){
         long wordsNum=0;
         int wordFlag=0;
-        StringBuilder tempWord=new StringBuilder();     //存单词
+        //StringBuilder tempWord=new StringBuilder();     //存单词
         for(int i=0;i<line.length();i++){
             if(Character.isLetter(line.charAt(i))){
                 wordFlag++;                             //如果是字母，将wordFlag++;
@@ -127,7 +99,7 @@ public class Lib {
     private static Map<String, Long> sortWord(Map<String,Long> map) {
         Map<String, Long> sorted = map.entrySet().stream()
                 .sorted(Map.Entry.<String, Long> comparingByValue().reversed()       //值升序排序
-                        .thenComparing(Map.Entry.comparingByKey()))                     //键排序
+                        .thenComparing(Map.Entry.comparingByKey())) .limit(10)                    //键排序
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
                         Map.Entry::getValue,
--- 221801312/src/WordCount.java
+++ 221801312/src/WordCount.java
@@ -1,8 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-import static java.util.Map.Entry.comparingByValue;
-import static java.util.stream.Collectors.toMap;
+
 
 public class WordCount{
     public String inputPath;
@@ -15,7 +14,7 @@ public class WordCount{
 
 
     //构造函数
-    public WordCount(String inputPath,String outputPath) throws IOException {
+    public WordCount(String inputPath,String outputPath) {
         this.inputPath=inputPath;
         this.outputPath=outputPath;
 
@@ -35,62 +34,45 @@ public class WordCount{
         fileUtil=new Lib.FileUtil(args[0]);
         BufferedReader reader=fileUtil.getReader();
 
-        Thread threadChars=new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    wordCount.characterNum=Lib.countChars(fileUtil.getReader());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        Thread threadChars=new Thread(() -> {
+            try {
+                wordCount.characterNum=Lib.countChars(fileUtil.getReader());
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         });
         threadChars.start();
 
-        Thread threadWords=new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    wordCount.validWord=Lib.countWords(fileUtil.getReader());
-                    wordCount.validLine=Lib.lineNum;
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        Thread threadWords=new Thread(() -> {
+            try {
+                wordCount.validWord=Lib.countWords(fileUtil.getReader());
+                wordCount.validLine=Lib.lineNum;
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         });
         threadWords.start();
 
-        Thread threadFrequency=new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    wordCount.mapWord=Lib.countWordFrequency(fileUtil.getReader());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        Thread threadFrequency=new Thread(() -> {
+            try {
+                wordCount.mapWord=Lib.countWordFrequency(fileUtil.getReader());
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         });
         threadFrequency.start();
-//        wordCount.characterNum=Lib.countChars(fileUtil.getReader());
-//        wordCount.validWord=Lib.countWords(fileUtil.getReader());
-//        wordCount.validLine=Lib.lineNum;
-//        wordCount.mapWord=Lib.countWordFrequency(fileUtil.getReader());
-//        while (true){
-//            if(Thread.activeCount()==1)
-//                break;
-//        }
 
         threadChars.join();
         threadFrequency.join();
         threadWords.join();
 
-        long endTime=System.currentTimeMillis();;   //获取程序结束时间
+        long endTime=System.currentTimeMillis();   //获取程序结束时间
         System.out.println("程序运行时间："+ (endTime-startTime)+"ms");
         System.out.println("单词："+wordCount.validWord);
         System.out.println("共"+wordCount.validLine+"行");
         System.out.println("字符数："+wordCount.characterNum);
 
-        Set<Map.Entry<String, Long>> entrySet= new HashSet<>();
+        Set<Map.Entry<String, Long>> entrySet;
         entrySet=wordCount.mapWord.entrySet();
         Iterator<Map.Entry<String, Long>> iter=entrySet.iterator();
         int l=0;                                                        //限制输出10次
@@ -103,25 +85,18 @@ public class WordCount{
 
     }
 
-
     public static void bigDateTest() throws IOException {
             StringBuilder stringBuilder = new StringBuilder();
-            //for(int k=0;k<100;k++){
-                for (int i = 0; i < 100000; i++) {
-                    stringBuilder.append("aaaa").append(i).append(",");
+            char c;
+            for(int i=0;i<=50000000;i++){
+                for(int j=0;j<(int)(Math.random()*10);j++){
+                    c=(char)(int)(Math.random()*26+97);
+                    stringBuilder.append(c);
                 }
-                stringBuilder.append('\n');
-                for (int i = 0; i < 100000; i++) {
-                    stringBuilder.append("bbbb").append(i).append(",");
-                }
-                stringBuilder.append('\n');
-                for (int i = 0; i < 10000; i++) {
-                    for (int j = 0; j < 100; j++) {
-                        stringBuilder.append("maxmax").append(j).append(",");
-                    }
+                stringBuilder.append((int)(Math.random()*1000)).append(',');
+                if((int)(Math.random()*100)==50)
                     stringBuilder.append('\n');
-                }
-            //}
+            }
             String testContent = stringBuilder.toString();
             System.out.println(testContent.length());
             BufferedWriter out=new BufferedWriter(new FileWriter("input2.txt"));
