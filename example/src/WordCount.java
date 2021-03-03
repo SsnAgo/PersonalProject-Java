78ee056009b54480cbfbc199aed5bba78876ae80
cqsg <137402865@qq.com>
2021-03-03 15:41:04 +0800

    Update WordCount.java

--- example/src/WordCount.java
+++ example/src/WordCount.java
@@ -1,9 +1,14 @@
-package wordcount;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;;
 
@@ -11,17 +16,54 @@ public class WordCount {
 	static HashMap<String, Integer > hashMap=new HashMap<String,Integer>();
     public static void main(String args[])
     {
-      	 String finname = new String("C:\\ccc\\s4.txt");
+      	 String finname = new String("C:\\ccc\\s5.txt");
+      	String foutname = new String("C:\\ccc\\s6.txt");
       	 //String foutname = args[1];
-      	 wordmanage(finname);
+      	 wordmanage(finname,foutname);
       	System.out.println("asda");
- 		Set<Map.Entry<String, Integer>> ms =hashMap.entrySet();
- 		for (Map.Entry entry : ms) {
- 			System.out.println(entry.getKey()+": "+entry.getValue());
- 		}
- 		
+      	mapmanage(foutname);
+      	
     }
-    public static void wordmanage(String finname) {
+    public static void mapmanage(String foutname) {
+    	List<Map.Entry<String,Integer>> list = new ArrayList<Map.Entry<String, Integer>>(hashMap.entrySet()); //将map的entryset放入list集合
+		//对list进行排序，并通过Comparator传入自定义的排序规则
+		Collections.sort(list,new Comparator<Map.Entry<String, Integer>>() {
+			@Override
+			public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+				int re = o2.getValue().compareTo(o1.getValue()); //重写排序规则，小于0表示升序，大于0表示降序
+				if(re!=0){
+					return re;
+					}else {
+						return o1.getKey().compareTo(o2.getKey());
+					}
+			}
+		});
+		for (int i = 0; i < list.size(); i++) {
+			String str=list.get(i).getKey() + ": " + list.get(i).getValue();
+			System.out.println(str);
+			outfile(foutname,str);
+        }    
+		
+    }
+    public static void outfile(String foutname,String str) {
+    	try 
+        {
+			BufferedWriter out = new BufferedWriter(new FileWriter(foutname,true));
+			out.write(str);
+			out.newLine();
+			out.close();
+		}
+		catch(ArrayIndexOutOfBoundsException ex) 
+        { 
+         System.out.println("\nPlease Enter the File Name in Command Line Argument. \n"+ 
+         "For Example :- java WordCount File1.txt File2.txt"); 
+        } 
+        catch(IOException ex) 
+        { 
+          System.out.println("File Does Not Found in given Directory. "); 
+        } 
+    }
+    public static void wordmanage(String finname,String foutname) {
     	try 
         {
     		 FileInputStream fr=new FileInputStream(new File(finname));
@@ -29,9 +71,10 @@ public class WordCount {
 	         char ch;
 	         int chcount=0;   //字符总数
 	         int wordcount=0;	//单词总数
-	         int rowcount=0;	//行总数
+	         int linecount=0;	//行总数
 	         int canbeword=0; 	//每次增加，当最后大于四的时候成立
 	         int canbetransferred=0;//可能为\n\r
+	         int linechcount=0;    //为空不能算一行
 	         String tempword ="";
 	         //每个单词一次循环
 	         while ((i=fr.read()) != -1){
@@ -39,7 +82,14 @@ public class WordCount {
 	        	 ch = (char)i;
 	        	 chcount++;  //字符统计
 	        	 if(ch=='\n') {
-	        		 rowcount++;
+	        		 /*
+	        		  * 如果linechcount==0，则说明换行前为空，空换行无效
+	        		  * 换行后把linechcount置为0
+	        		  */
+	        		 if(linechcount!=0) {
+	        			 linecount++;
+	        		 }
+	        		 linechcount=0;
 	        	 }
 	        	 if (canbetransferred == 1){
 	        	/* 处理\n\r
@@ -53,15 +103,21 @@ public class WordCount {
 	        	 * 2、将进入下一层判断首字母
 	        	 */
 	        		 canbetransferred=0;
+	        		 if (ch == 'n'){
+	        			 chcount--;
+	        			 if(linechcount!=0) {
+		        			 linecount++;
+		        		 }
+		        		 linechcount=0;
+	        			 continue;
+	        		 }
+	        		 
+	        		 linechcount++;//不是换行符就++
 	        		 if (ch == 'r'){
 	        			 chcount--;
 	        			 continue;
 	        		 }
-	        		 if (ch == 'n'){
-	        			 chcount--;
-	        			 rowcount++;
-	        			 continue;
-	        		 }
+
 	        	 }
 	        	 if (!Character.isLetter(ch) && !Character.isDigit(ch)){
 	        		 /* 分割符
@@ -92,11 +148,13 @@ public class WordCount {
         			 canbeword=0;
 	        	 }else if (Character.isLetter(ch)){
 	        		 canbeword++;
+	        		 linechcount++;
 	        		 if(Character.isUpperCase(ch)) {
 	        			 ch = Character.toLowerCase(ch);
 	        		 }
 	        		 tempword+=ch;
 	        	 }else if(Character.isDigit(ch)) {
+	        		 linechcount++;
 	        		 if (canbeword < 4) {
 	        			 canbeword=0;
 	        			 tempword="";
@@ -114,10 +172,11 @@ public class WordCount {
     			 }
     			 wordcount++;
     		 }
+	         if(linechcount>0) {
+	        	 linecount++;
+	         }
 	         fr.close(); 
-	         System.out.println("characters: "+chcount);	       
-	         System.out.println("words: "+wordcount);
-	         System.out.println("lines: "+rowcount);
+	         outfile(foutname,"characters: "+chcount+'\n'+"words: "+wordcount+'\n'+"lines: "+linecount);
         } 
         catch(ArrayIndexOutOfBoundsException ex) 
         { 
