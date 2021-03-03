a4e5453a8aaf859a5f6a65bc5e7aeed0a6a7dee0
aaagx <54830165+aaagx@users.noreply.github.com>
2021-03-03 23:03:34 +0800

    进行单元测试与性能测试后完成了主函数
    这版已经可以完成任务了 还没有改进性能

--- 081700318/src/WordCount.java
+++ 081700318/src/WordCount.java
@@ -4,7 +4,16 @@ import java.io.Writer;
 
 public class WordCount{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        FileTool FileIo=new FileTool();
+        FileIo.getReader(args[0]);
+        FileIo.getWriter(args[1]);
+        ComputeTool ComputeCore=new ComputeTool(FileIo.getFileString());
+        ComputeCore.compute();
+        FileIo.writeResult(ComputeCore.CharNums,ComputeCore.RowNums,ComputeCore.WordNums,ComputeCore.getTOPWords(10));
+        FileIo.closeReader();
+        FileIo.closeWriter();
     }
 
     }
