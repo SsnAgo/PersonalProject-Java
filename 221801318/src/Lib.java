c9596f0fcbc412e7adba076735bdaaaab6ac111f
h2333 <1106757248@qq.com>
2021-03-04 00:40:27 +0800

    第三次提交
    此次提交完善统计文章字符数的功能，测试时发现若最后一行为空行时，测试结果会比实际值小一。经研究发现是使用readline()方法按行读取时若最后一行为空行则被判断为空。故修改按行读取的方式为按字节读取。具体实现详见博客。

--- 221801318/src/Lib.java
+++ 221801318/src/Lib.java
@@ -8,37 +8,61 @@ public class Lib {
 
     private int CHARACTERS_NUM = 0;
 
-    final private String CHARACTER_REGEX = "[^a-zA-Z0-9\t\n\\s]";
+    final private String CHARACTER_REGEX = "[^a-zA-Z0-9\\t\\n\\s]";
 
 
     public String returnCharacters(String filePath)
     {
-        String stream = "";
-        int line = 0;
-        try {
-            FileInputStream fileInputStream = new FileInputStream(filePath);
-            BufferedReader br = new BufferedReader(new InputStreamReader(fileInputStream));
-            String temp = "";
 
-            try {
+        //该实现方法在最后一行为空行时，会导致结果出问题。
+//        String stream = "";
+//        int line = 0;
+//        FileInputStream fileInputStream = null;
+//        try {
+//            fileInputStream = new FileInputStream(filePath);
+//            BufferedReader br = new BufferedReader(new InputStreamReader(fileInputStream));
+//            String temp = "";
+//            try {
                 //从BufferReader中读取下一行
-                while ((temp = br.readLine()) != null){
-                    //读取到的文件信息
-                    stream = stream + temp;
-                    line ++;
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } catch (FileNotFoundException e){
-            e.printStackTrace();
-        }
+//                while ((temp = br.readLine()) != null){
+//                    //读取到的文件信息
+//                    stream = stream + temp;
+//                    line ++;
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
 //        if(line == 1){
 //            line = 0;
 //        }
-        System.out.println(LINE+line);
-        stream = stream.replaceAll(CHARACTER_REGEX,"");
-        CHARACTERS_NUM = stream.length() + line;
+//        System.out.println(LINE+line);
+//        stream = stream.replaceAll(CHARACTER_REGEX,"");
+//        CHARACTERS_NUM = stream.length() + line;
+
+        byte[] stream = new byte[20*1024];
+        int len = stream.length;
+        String temp = "";
+        String result = "";
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(filePath);
+            while (fileInputStream.read(stream,0,len) != -1){
+                temp = new String(stream);
+                result += temp;
+            }
+            result = result.replaceAll(CHARACTER_REGEX,"");
+            CHARACTERS_NUM += result.length();
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fileInputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         System.out.println(CHARACTERS_NUM);
 
