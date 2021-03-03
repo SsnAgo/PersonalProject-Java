0e9ae435f26a63e8feed8c642a47767c24d025ef
fzu-Alex <1217812752@qq.com>
2021-03-03 13:26:06 +0800

    11.0版本
    完成了单元测试，优化了部分代码，删去不必要的判断与变量。
    添加了readme（最终版）

--- 221801128/src/Lib.java
+++ 221801128/src/Lib.java
@@ -1,22 +1,18 @@
-
 import java.io.IOException;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 public class Lib {
-
 	static HashMap<String, Integer> hash=new HashMap<String,Integer>();
-
 	/**
 	 * 统计单词并存储
 	 */
 	public static int countWords(String str) {
 		int cnt = 0;
 		Pattern pattern = Pattern.compile("^[a-z]{4}[a-z0-9]*");
+		String[] wordStr = str.split("[^a-zA-Z0-9]");
 		Matcher matcher = null;
-		String[] wordStrTmp = str.split("[^a-zA-Z0-9]");
-		for (String word : wordStrTmp) {
+		for (String word : wordStr) {
 			word = word.toLowerCase();
 			matcher = pattern.matcher(word);
 			if (!word.equals("") && matcher.find()) {
@@ -56,8 +52,7 @@ public class Lib {
 		 * 按词频以及字典序排序
 		 */
 		public static List<HashMap.Entry<String, Integer>> getSortedList(HashMap<String, Integer> wordMap) {
-			List<HashMap.Entry<String, Integer>> list =
-					new ArrayList<HashMap.Entry<String, Integer>>(wordMap.entrySet());
+			List<HashMap.Entry<String, Integer>> list = new ArrayList<HashMap.Entry<String, Integer>>(wordMap.entrySet());
 			Collections.sort(list, new Comparator<Map.Entry<String,Integer>>() {
 				public int compare(Map.Entry<String,Integer> o1, Map.Entry<String,Integer> o2){
 					if(o1.getValue().equals(o2.getValue()))
@@ -67,10 +62,4 @@ public class Lib {
 			});
 			return list;
 		}
-
-
-
-
-	 
-
 }
--- 221801128/src/WordCount.java
+++ 221801128/src/WordCount.java
@@ -1,59 +1,42 @@
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 
 public class WordCount {
-	private static String textStr;
 	private static String inputfile;
     private static String outputfile;
-	public WordCount(String inputPath, String outputPath) throws IOException {
-        this.inputfile = inputPath;
-        this.outputfile = outputPath;
-		textStr = readFile(inputPath);
-    }
 
 	/**
 	 * 读取指定文件，返回字符串
 	 */
-	public String readFile(String file) throws IOException {
+	public static String readFile(String file) throws IOException {
 		BufferedReader br = null;
 		StringBuilder str = new StringBuilder();
 		int ch;
-		try {
-			br = new BufferedReader(new FileReader(file));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+		br = new BufferedReader(new FileReader(file));
+		while (true) {
+			assert br != null;
+			if (!((ch = br.read()) != -1)) break;
+			str.append((char)ch);
 		}
-		try {
-			while (true) {
-				assert br != null;
-				if (!((ch = br.read()) != -1)) break;
-				str.append((char)ch);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			br.close();
-		}
+		br.close();
 		return str.toString();
 	}
 
 	/**
 	 * 写入指定文件
 	 */
-	public static void writeIn(String outputfile) throws IOException {
-		Path path1 = Paths.get(outputfile);
+	public static void writeIn(String inputfile,String outputfile) throws IOException {
 		BufferedWriter writer = null;
-		StringBuilder str = new StringBuilder("characters: " + Lib.countChar(textStr) + "\n"
-				+ "words: " + Lib.countWords(textStr) + "\n"
-				+ "lines: " + Lib.countLines(textStr) + "\n");
+		StringBuilder str = new StringBuilder("characters: " + Lib.countChar(readFile(inputfile)) + "\n"
+				+ "words: " + Lib.countWords(readFile(inputfile)) + "\n"
+				+ "lines: " + Lib.countLines(readFile(inputfile)) + "\n");
 		int cnt = 0;
 		try {
-			writer = Files.newBufferedWriter(path1, StandardCharsets.UTF_8);
+			writer = Files.newBufferedWriter(Paths.get(outputfile), StandardCharsets.UTF_8);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -63,9 +46,9 @@ public class WordCount {
 		for(HashMap.Entry<String,Integer> entry:sortedList) {
 			str.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
 			cnt++;
-			if (cnt >= 10) break;
+			if (cnt >= 10)
+				break;
 		}
-		assert writer != null;
 		writer.write(str.toString());
 		writer.close();
 	}
@@ -75,9 +58,9 @@ public class WordCount {
 	 * @throws IOException
 	 */
 //	public static void printall() throws IOException {
-//		StringBuilder str = new StringBuilder("characters: " + Lib.countChar(textStr) + "\n"
-//				+ "words: " + Lib.countWords(textStr) + "\n"
-//				+ "lines: " + Lib.countLines(textStr) + "\n");
+//		StringBuilder str = new StringBuilder("characters: " + Lib.countChar(readFile(inputfile)) + "\n"
+//				+ "words: " + Lib.countWords(readFile(inputfile)) + "\n"
+//				+ "lines: " + Lib.countLines(readFile(inputfile)) + "\n");
 //		int cnt = 0;
 //		List<HashMap.Entry<String, Integer>> sortedList = Lib.getSortedList(Lib.hash);
 //		for(HashMap.Entry<String,Integer> entry:sortedList) {
@@ -92,17 +75,13 @@ public class WordCount {
             System.out.println("需要两个参数，请重新输入！");
             return;
         }
-        WordCount wordcount = new WordCount(args[0],args[1]);
-        
 		File dir = new File(".");
-		inputfile = dir.getCanonicalPath()+"\\"+inputfile;
-		outputfile = dir.getCanonicalPath()+"\\"+outputfile;
+		inputfile = dir.getCanonicalPath()+"\\"+args[0];
+		outputfile = dir.getCanonicalPath()+"\\"+args[1];
 		//System.out.println("读取文件的地址："+inputfile);
-
-
 		//printall();
 		//写入指定文件
-		writeIn(outputfile);
+		writeIn(inputfile,outputfile);
 	}
 }
 
