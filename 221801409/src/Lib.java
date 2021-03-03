13c61d3a990987d65b63ddf974f469f85be58b6c
LiarX <58362731+LiarX@users.noreply.github.com>
2021-03-03 22:32:14 +0800

    第五次commit
    修改了单词统计只统计前三个字母是否为英文字母的bug，新增统计单词频率和按值排序单词方法

--- 221801409/src/Lib.java
+++ 221801409/src/Lib.java
@@ -1,53 +1,89 @@
 import java.io.*;
+import java.util.*;
 
 public class Lib {
-    public static String readFormTxt(String txtPath){
-        File file= new File(txtPath);
-        StringBuilder result=new StringBuilder();
-        try{
-            BufferedReader br=new BufferedReader(new FileReader(file));
-            String str=null;
-            while((str=br.readLine())!=null){
-                result.append(str+"\n");
+    public static String readFormTxt(String txtPath) {
+        File file = new File(txtPath);
+        StringBuilder result = new StringBuilder();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String str = null;
+            while ((str = br.readLine()) != null) {
+                result.append(str + "\n");
             }
             br.close();
-        }catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return result.toString();
     }
-    public static int getCharactersCount(String str){
-        int count=0;
-        char[] ch=str.toCharArray();
-        for(int i=0;i<ch.length;i++){
-            if(ch[i]>=0&&ch[i]<128){
+
+    public static int getCharactersCount(String str) {
+        int count = 0;
+        char[] ch = str.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] >= 0 && ch[i] < 128) {
                 count++;
             }
             //只计算ACSII字符
         }
         return count;
     }
-    public static int getWordsCount(String str){
-        int count=0;
-        String temp=null;
-        String regex="^[a-z]+$";
-        str=str.toLowerCase();//将字符串变成小写
-        String[] strArray=str.split("[^a-z0-9]+");
+
+    public static int getWordsCount(String str) {
+        int count = 0;
+        String temp = null;
+        String regex = "^[a-z]+$";
+        str = str.toLowerCase();//将字符串变成小写
+        String[] strArray = str.split("[^a-z0-9]+");
         //用split方法将非字母数字的字符作为分割线分割成单词数组，字符串已经是小写了，不存在A-Z
-        for(int i=0;i<strArray.length;i++){
-            if(strArray[i].length()>=4){//至少要四个英文字母开头，也就是说单词至少需要四个字符
-                temp=strArray[i].substring(0,3);//取出分割后字符串的前四个字符
-                if(temp.matches(regex))
+        for (int i = 0; i < strArray.length; i++) {
+            if (strArray[i].length() >= 4) {//至少要四个英文字母开头，也就是说单词至少需要四个字符
+                temp = strArray[i].substring(0, 4);//取出分割后字符串的前四个字符
+                if (temp.matches(regex))
                     count++;
             }
         }
         return count;
     }
-    public static int getLineCount(String str){
-        str=str.replaceAll("[^\\S\\r\\n]","");//将str中除了换行符以外的空白字符删掉
-        String count[]=str.split("\r|\n|\r\n");//将新字符串以split方法进行换行符分割后，利用length方法统计行数
+
+    public static int getLineCount(String str) {
+        str = str.replaceAll("[^\\S\\r\\n]", "");//将str中除了换行符以外的空白字符删掉
+        String count[] = str.split("\r|\n|\r\n");//将新字符串以split方法进行换行符分割后，利用length方法统计行数
         return count.length;
+    }
+
+    public static List getWordFrequency(String str) {
+        HashMap map = new HashMap<>();
+        String temp = null;
+        String regex = "^[a-z]+$";
+        str = str.toLowerCase();//将字符串变成小写
+        String[] strArray = str.split("[^a-z0-9]+");
+        //用split方法将非字母数字的字符作为分割线分割成单词数组，字符串已经是小写了，不存在A-Z
+        for (int i = 0; i < strArray.length; i++) {
+            if (strArray[i].length() >= 4) {//至少要四个英文字母开头，也就是说单词至少需要四个字符
+                temp = strArray[i].substring(0, 4);//取出分割后字符串的前四个字符
+                if (temp.matches(regex)) {
+                    int freq = map.get(strArray[i]) == null ? 0 : (int) map.get(strArray[i]);
+                    map.put(strArray[i], freq == 0 ? 1 : freq + 1);//利用hashmap，将单词作为键，出现次数作为值
+                }
+            }
+        }
+        //实现按值排序
+        List<Map.Entry<String, Integer>> list = new ArrayList<Map.Entry<String, Integer>>(map.entrySet());//将entrySet转换为List
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {//修改比较器达到按值降序排序目的
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
+        //同值情况按字典排序
+
+        for(int i=0;i<10;i++){
+            System.out.println(list.get(i).getKey()+": "+list.get(i).getValue());
+        }
+        return list;
     }
 }
--- 221801409/src/WordCount.java
+++ 221801409/src/WordCount.java
@@ -4,5 +4,6 @@ public class WordCount {
         System.out.println(Lib.getCharactersCount(Lib.readFormTxt("D:/123.txt")));
         System.out.println(Lib.getWordsCount(Lib.readFormTxt("D:/123.txt")));
         System.out.println(Lib.getLineCount(Lib.readFormTxt("D:/123.txt")));
+        System.out.println(Lib.getWordFrequency(Lib.readFormTxt("D:/123.txt")));
     }
 }
