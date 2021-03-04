ee8fc369a76bba20a14ad1eb7081f57b9f9022df
Ye777 <58556652+Ye777@users.noreply.github.com>
2021-03-04 16:56:53 +0800

    实现了统计字符数功能

---
+++ 221801434/src/WordCount.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.Map;
+
+import java.util.TreeMap;
+public class WordCount {
+    private static Map<String, Object> word = new TreeMap<String, Object>();
+    public static void main(String[] args) throws IOException{
+        // write your code here
+        System.out.println("hello");
+        System.out.println(countChar("C:\\Users\\Scenario\\Desktop\\test.txt"));
+    }
+    public static int countChar(String filepath)throws IOException {
+        BufferedReader reader=new BufferedReader(new FileReader(filepath));
+        if(!reader.ready())
+        {
+            System.out.println("文件流无法读取");
+        }
+        int num=0;
+        String line=null;
+        while((line=reader.readLine())!=null)
+        {
+            num+=line.length();
+        }
+        reader.close();
+        return num;
+
+    }
+
+}
