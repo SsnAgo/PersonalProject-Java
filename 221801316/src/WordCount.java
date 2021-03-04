a1eccb762b010af18d7d1482a007a0f12e5591a0
Liangdayi <1113901523@qq.com>
2021-03-04 22:51:15 +0800

    功能整合

--- 221801316/src/WordCount.java
+++ 221801316/src/WordCount.java
@@ -1,13 +1,27 @@
+
 import java.io.*;
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 class WordCount{
+
     public static void main(String[] args) {
         File inputFile=new File(args[0]);
         File outputFile=new File(args[1]);
+        StringBuilder content=new StringBuilder();
         int characterCount= CountCore.characterCount(inputFile);
-        System.out.println(characterCount);
-        CountCore.toLowerCase(inputFile);
+        content.append("characters:"+characterCount+"\n");
+        int words=CountCore.wordCount(inputFile);
+        content.append("words:"+words+"\n");
+        int lines=CountCore.lineCount(inputFile);
+        content.append("lines:"+lines+"\n");
+        Map<String,Integer> map=CountCore.wordStore(inputFile);
+        List<Map.Entry<String,Integer>> list=CountCore.sortByFrequency(map);
+        for(int i=0;i<10&&i<list.size();i++){
+            content.append(list.get(i).getKey()+":"+list.get(i).getValue()+"\n");
+        }
+        FileIO.writeToFile(outputFile,content.toString());
     }
 
 }
