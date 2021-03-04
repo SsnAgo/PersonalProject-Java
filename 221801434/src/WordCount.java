866fecafb68483cf6b35d2b0b5c606b7103ea66a
Ye777 <58556652+Ye777@users.noreply.github.com>
2021-03-04 16:58:57 +0800

    实现了统计行数功能

--- 221801434/src/WordCount.java
+++ 221801434/src/WordCount.java
@@ -8,6 +8,7 @@ public class WordCount {
         // write your code here
         System.out.println("hello");
         System.out.println(countChar("C:\\Users\\Scenario\\Desktop\\test.txt"));
+        System.out.println(countLine("C:\\Users\\Scenario\\Desktop\\test.txt"));
     }
     public static int countChar(String filepath)throws IOException {
         BufferedReader reader=new BufferedReader(new FileReader(filepath));
@@ -24,6 +25,21 @@ public class WordCount {
         reader.close();
         return num;
 
+    }
+    public static int countLine(String filepath)throws IOException {
+        BufferedReader reader=new BufferedReader(new FileReader(filepath));
+        if(!reader.ready())
+        {
+            System.out.println("文件流无法读取");
+        }
+        int num=0;
+        String line=null;
+        while((line=reader.readLine())!=null)
+        {
+            num+=1;
+        }
+        reader.close();
+        return num;
     }
 
 }
