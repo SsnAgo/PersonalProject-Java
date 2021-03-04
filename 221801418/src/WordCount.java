f4817399a9ca1a79d82390fa59bedf842d0dc7b1
kepachirenguai <79777840+kepachirenguai@users.noreply.github.com>
2021-03-04 21:42:29 +0800

    update
    按照codestyle把代码检查规范了一下

--- 221801418/src/WordCount.java
+++ 221801418/src/WordCount.java
@@ -1,11 +1,12 @@
 import java.io.IOException;
 
 public class WordCount {
-    public static void main(String argv[]) throws IOException{
-    	Lib lib=new Lib();
-        String str=lib.readTextFile(argv[0]);
-        if(!str.isEmpty()) {
-        	lib.writeTextFile(Lib.countChars(str),Lib.countWords(str),Lib.countLines(str),lib.countTimes(str),argv[1]);
-        }
-    }  
+	public static void main(String argv[]) throws IOException {
+		Lib lib=new Lib();
+		String str=lib.readTextFile(argv[0]);
+		if(!str.isEmpty()) {
+			lib.writeTextFile(Lib.countChars(str),Lib.countWords(str),
+					Lib.countLines(str),lib.countTimes(str),argv[1]);
+		}
+	}  
 }
