21c138af9488f5259d6f6b8c9d2ba5f615930c0e
Kosmos5 <1821748823@qq.com>
2021-03-04 22:01:38 +0800

    Update WordCount.java
    执行WordCount函数 完成主函数

--- 221801422/src/WordCount.java
+++ 221801422/src/WordCount.java
@@ -21,4 +21,31 @@ public class WordCount {
         this.chararacter = Lib.getCharCount(Lib.readFromFile(this.inputPath));
         this.word = Lib.getWordCount(Lib.readFromFile(this.inputPath));
     }
+
+    public void runWordCount()
+    {
+        //数据输出到文件
+        Lib.writeToFile(this.character, this.word, this.line, this.outputPath);
+    }
+
+    public static void main(String[] args)
+    {
+        //命令行参数判断
+        if(args.length < 2)
+        {
+            System.out.println("参数不足, 请重新运行");
+        }
+        else
+        {
+            //记录时间戳
+            long start = System.currentTimeMillis();
+            //两个文件路径
+            WordCount wordCount = new WordCount(args[0], args[1]);
+            wordCount.init();
+            wordCount.runWordCount();
+            long end = System.currentTimeMillis();
+            //获取代码执行时长
+            System.out.println("time costs: " + (end - start) + "ms");
+        }
+    }
 }
