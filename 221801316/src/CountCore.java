562ccb8626e00af19b8ac29b64c494b992f4fa1a
Liangdayi <1113901523@qq.com>
2021-03-04 23:50:35 +0800

    Update CountCore.java
    一开始构思以为需要先对文本进行小写转化，故先写了该方法，后面发现这个功能在后续的统计中有体现，于是将其删去

--- 221801316/src/CountCore.java
+++ 221801316/src/CountCore.java
@@ -21,25 +21,6 @@ public class CountCore {
         }
         return characterCount;
     }
-
-    public static void toLowerCase(File inputFile){
-        BufferedReader bufferedReader= null;
-        StringBuilder stringBuilder=new StringBuilder();
-        try {
-            bufferedReader = new BufferedReader(new FileReader(inputFile));
-            String str;
-            while((str=bufferedReader.readLine())!=null){
-                stringBuilder.append(str.toLowerCase());
-                stringBuilder.append("\r\n");
-            }
-            bufferedReader.close();
-            System.out.println(stringBuilder.toString());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-    }
     public static boolean isWord(String string){
         String wordRegex="[a-z]{4}[a-z0-9]*";
         return Pattern.matches(wordRegex,string);
