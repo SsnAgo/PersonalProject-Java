72fd1d190cff9b04cf52abd719dd223e3c5b8136
Ye777 <58556652+Ye777@users.noreply.github.com>
2021-03-04 17:01:17 +0800

    实现了输出数量前十的单词功能

--- 221801434/src/WordCount.java
+++ 221801434/src/WordCount.java
@@ -10,6 +10,7 @@ public class WordCount {
         System.out.println(countChar("C:\\Users\\Scenario\\Desktop\\test.txt"));
         System.out.println(countLine("C:\\Users\\Scenario\\Desktop\\test.txt"));
         System.out.println(countWord("C:\\Users\\Scenario\\Desktop\\test.txt"));
+        countkeyword("C:\\Users\\Scenario\\Desktop\\test.txt");
     }
     public static int countChar(String filepath)throws IOException {
         BufferedReader reader=new BufferedReader(new FileReader(filepath));
@@ -69,6 +70,59 @@ public class WordCount {
             num++;
         }
         return num;
+    }
+    public static  void countkeyword(String filepath)throws IOException {
+        int n=countWord(filepath);
+        int N=10;
+        BufferedReader reader=new BufferedReader(new FileReader(filepath));
+        if(!reader.ready())
+        {
+            System.out.println("文件流无法读取");
+        }
+        int num=0;
+        String line=null;
+        while((line=reader.readLine())!=null)
+        {
+            String s1 = line.replaceAll("\\W", " ");
+            String s2 = s1.replaceAll(" +", " ");
+            String []s3=s2.split(" ");
+            for(String a:s3)
+            {
+                if(a.length()>3&&(a.charAt(0)<'0'||a.charAt(0)>'9'))
+                {
+                    int number=(int)word.get(a);
+                    word.put(a,number+1);
+
+                }
+            }
+
+        }
+        for(int i=0;i<=N;i++)
+        {
+            int max=0;
+            String maxs=null;
+            for (String key : word.keySet()) {
+                if(max<(int)word.get(key))
+                {
+                    max=(int)word.get(key);
+                    maxs=key;
+
+                }
+                if(max==(int)word.get(key))
+                {
+                    if(maxs.compareTo(key)>0)
+                    {
+                        max=(int)word.get(key);
+                        maxs=key;
+                    }
+                }
+            }
+            System.out.println(maxs+":"+max);
+            word.remove(maxs);
+            n--;
+            if(n<=0) break;
+        }
+
     }
 
 }
