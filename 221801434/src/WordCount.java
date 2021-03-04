eda2cf51ac319dcd8878c141b05608fc7095687f
Ye777 <58556652+Ye777@users.noreply.github.com>
2021-03-04 16:59:55 +0800

    实现了统计单词数量的功能

--- 221801434/src/WordCount.java
+++ 221801434/src/WordCount.java
@@ -9,6 +9,7 @@ public class WordCount {
         System.out.println("hello");
         System.out.println(countChar("C:\\Users\\Scenario\\Desktop\\test.txt"));
         System.out.println(countLine("C:\\Users\\Scenario\\Desktop\\test.txt"));
+        System.out.println(countWord("C:\\Users\\Scenario\\Desktop\\test.txt"));
     }
     public static int countChar(String filepath)throws IOException {
         BufferedReader reader=new BufferedReader(new FileReader(filepath));
@@ -39,6 +40,34 @@ public class WordCount {
             num+=1;
         }
         reader.close();
+        return num;
+    }
+    public static int countWord(String filepath)throws IOException {
+        BufferedReader reader=new BufferedReader(new FileReader(filepath));
+        if(!reader.ready())
+        {
+            System.out.println("文件流无法读取");
+        }
+        int num=0;
+        String line=null;
+        while((line=reader.readLine())!=null)
+        {
+            String s1 = line.replaceAll("\\W", " ");
+            String s2 = s1.replaceAll(" +", " ");
+            String []s3=s2.split(" ");
+            for(String a:s3)
+            {
+                if(a.length()>3&&(a.charAt(0)<'0'||a.charAt(0)>'9'))
+                {
+                    word.put(a,0);
+
+                }
+            }
+
+        }
+        for (String key : word.keySet()) {
+            num++;
+        }
         return num;
     }
 
