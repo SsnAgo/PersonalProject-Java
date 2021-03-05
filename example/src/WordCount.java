85dde4aa76c27731f2ccfc79fdfb1b7e509c563e
221801333lsl <1390476733@qq.com>
2021-03-05 17:44:52 +0800

    Update WordCount.java
    将大致需要实现的函数罗列出来

---
+++ example/src/WordCount.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.util.Map;
+
+public class Count {
+	//定义三个函数，分别用来将字符数、单词数、有效行数、十个词频最高的单词写入文件
+	void countChar(String fileName) 
+	{
+		int count_char=0;
+	}
+	void countWord(String fileName)
+	{
+		int count_word=0;
+	}
+	void countLine(String fileName) 
+	{
+		int count_line=0;
+	}
+	void countWordFrequency(String fileName)
+	{
+		
+	}
+}
