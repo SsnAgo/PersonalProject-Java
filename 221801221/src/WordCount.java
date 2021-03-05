535071c2758e3d07ebe911ecc095aa4bb68f730c
GONGHAIXU <3111398755@qq.com>
2021-03-05 15:29:14 +0800

    Change regex

--- 221801221/src/WordCount.java
+++ 221801221/src/WordCount.java
@@ -12,14 +12,13 @@ public class WordCount {
         WordCount wordCount = new WordCount(fileContent);
         StringBuilder outputFileContentBuilder = new StringBuilder();
         outputFileContentBuilder.append("characters: " + wordCount.countCharNum() + "\n");
-        outputFileContentBuilder.append("words: " + wordCount.countWords() + "\n");
-        outputFileContentBuilder.append("lines: " + wordCount.countLInes() + "\n");
+        outputFileContentBuilder.append("words: " + wordCount.countWord() + "\n");
+        outputFileContentBuilder.append("lines: " + wordCount.countLIne() + "\n");
         for (WordEntry e:
                 wordCount.getTop10WordsByFrequencyOfAppearance()) {
             outputFileContentBuilder.append(e.key + ": " + e.value + "\n");
         }
         FileUtil.outputStringToFile(args[1],outputFileContentBuilder.toString());
-
     }
 
     public WordCount(String fileContent){
@@ -30,8 +29,8 @@ public class WordCount {
         return inputFileContent.length();
     }
 
-    public int countWords(){
-        String[] words = inputFileContent.split("[^0-9|^A-z]");
+    public int countWord(){
+        String[] words = inputFileContent.split("\\W+");
         int wordsNum = words.length;
         for (String word:
                 words) {
@@ -40,7 +39,7 @@ public class WordCount {
         return wordsNum;
     }
 
-    public int countLInes(){
+    public int countLIne(){
         String[] lines = inputFileContent.split("\n");
         int linesNum = lines.length;
         for (String line:
@@ -53,7 +52,7 @@ public class WordCount {
 
     public List<WordEntry<String,Integer>> getTop10WordsByFrequencyOfAppearance(){
         HashMap<String,WordEntry<String,Integer>> wordMap = new HashMap<>();
-        String[] words = inputFileContent.split("[^0-9|^A-z]");
+        String[] words = inputFileContent.split("\\W+");
         ArrayList<WordEntry<String,Integer>> wordEntries = new ArrayList<>();
         HashMap<String,WordEntry<String,Integer>> wordEntryHashMap = new HashMap<>();
         for (String word:
@@ -68,8 +67,18 @@ public class WordCount {
             }
         }
         Collections.sort(wordEntries,(WordEntry<String,Integer> e1, WordEntry<String,Integer> e2) ->
-                e1.value == e2.value ? e2.key.compareTo(e1.key) : e2.value - e1.value);
+                e1.value == e2.value ? e1.key.compareTo(e2.key) : e2.value - e1.value);
         return wordEntries.size() >= 10 ? wordEntries.subList(0,10) : wordEntries.subList(0, wordEntries.size());
+    }
+
+    public String getTop10WordsByFrequencyOfAppearanceInString(){
+        List<WordEntry<String,Integer>> top10 = getTop10WordsByFrequencyOfAppearance();
+        StringBuilder res = new StringBuilder();
+        for (WordEntry e:
+             top10) {
+            res.append(e.key + ": " + e.value + "\n");
+        }
+        return  res.toString();
     }
 
     private class WordEntry<K,V>{
