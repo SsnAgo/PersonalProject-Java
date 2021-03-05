209a270509fd3ffaea4d501ceb759866ed282b60
Fino123 <965200752@qq.com>
2021-03-05 11:12:05 +0800

    分离出文件读取功能fileToString，编写判断单词功能isWord

--- 221801435/src/Lib.java
+++ 221801435/src/Lib.java
@@ -15,35 +15,18 @@ public class Lib {
     /**
      * 编写者：221801435
      * 功能：统计文件的Ascii字符数
-     * @param file_path 文件路径
+     * @param file_info 文件信息字符串
      * @return Ascii字符数
      */
-    public int getAsciiCount(String file_path){
+    public int getAsciiCount(String file_info){
         int counter = 0;
-        InputStream in = null;
-        try {
-            in = new FileInputStream(file_path);
-            int c = in.read();
-            while (c!=-1){
-                if (counter>=0&&counter<=127)
-                    counter++;
-                c = in.read();
-            }
-        }catch (FileNotFoundException e){
-            System.err.println(file_path+"文件不存在，请检查路径！");
-        }catch (IOException e){
-            System.err.println(file_path+"文件打开失败");
-        }catch (Exception e){
-            System.err.println("未知错误");
-        }finally {
-            if (in != null){
-                try {
-                    in.close();
-                }catch (IOException e){
-                    System.err.println(file_path+"文件关闭失败");
-                }
+        for (int i=0;i<file_info.length();i++){
+            int c = (int)file_info.charAt(i);
+            if (c>=0&&c<=127){
+                counter++;
             }
         }
+        System.out.println(file_info);
         return counter;
     }
 
@@ -54,6 +37,8 @@ public class Lib {
      * @return int 文件行数
      */
     public int getLinesCount(String file_path){
+        String file_info = fileToString(file_path);
+
         BufferedReader reader = null;
         int counter = 0;
         try {
@@ -85,6 +70,7 @@ public class Lib {
      * @return int 文件单词数
      */
     public int getWordsCount(String file_path){
+        StringBuilder builder = new StringBuilder();
         return 0;
     }
 
@@ -96,5 +82,59 @@ public class Lib {
      */
     public Map<String,Integer> getMostFrequentlyWords(String file_path){
         return null;
+    }
+
+    /**
+     * 编写者: 221801435
+     * 功能: 判断字符串是否是单词,单词：至少以4个英文字母开头，跟上字母数字符号，单词以分隔符分割，不区分大小写
+     * @param word 单词字符串
+     * @return 判断结果，是单词返回True，否则返回False
+     */
+    private boolean isWord(String word){
+        if (word.length()<4){
+            return false;
+        }
+        //不区分大小写
+        String test = word.toLowerCase();
+        //前4位是字母
+        for(int i=0;i<4;i++){
+            char c = test.charAt(i);
+            if (!(c>='a'&&c<='z')){
+                return false;
+            }
+        }
+        //后跟字母数字符号
+        for(int i=3;i<word.length();i++){
+            if(!Character.isDigit(test.charAt(i))&&!Character.isAlphabetic(test.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private String fileToString(String file_path){
+        BufferedReader reader = null;
+        StringBuilder builder = new StringBuilder();
+        try {
+            reader = new BufferedReader(new InputStreamReader(new FileInputStream(file_path)));
+            int c;
+            while ((c=reader.read())!=-1){
+                builder.append((char)c);
+            }
+        }catch (FileNotFoundException e){
+            System.err.println(file_path+"文件不存在");
+        }catch (IOException e){
+            System.err.println(file_path+"文件打开失败");
+        }
+        finally {
+            if (reader!=null){
+                try {
+                    reader.close();
+                }catch (IOException e){
+                    System.err.println(file_path+"文件关闭失败");
+                }
+            }
+        }
+        return builder.toString();
     }
 }
