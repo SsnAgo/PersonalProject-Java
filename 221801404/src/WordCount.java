43d2c30774c3ba91f2e7ecc53375ea5c59a63a8a
weihaochen111 <786576329@qq.com>
2021-03-05 13:56:44 +0800

    将排好序的单词依次输出

--- 221801404/src/WordCount.java
+++ 221801404/src/WordCount.java
@@ -1,12 +1,12 @@
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
+import java.util.Map;
 
 public class WordCount
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws IOException {
         String filePath = args[0];
+        String outFilePath = args[1];
         String content = Lib.readFile(filePath);
         int charactersNum = Lib.getCharacterNum(content);
         int linesNum = Lib.getLinesNum(content);
@@ -14,6 +14,25 @@ public class WordCount
         Lib lib = new Lib();
         List list = lib.sortMap();
         System.out.println("charactersNum: "+charactersNum+"\nlinesNum: "+linesNum+"\n");
-        System.out.println(list);
+        //System.out.println(list);
+        //输出
+        List<Map.Entry<String,Integer>> wordsMap=lib.sortMap();
+        FileOutputStream fileOutputStream=new FileOutputStream(outFilePath);
+        OutputStreamWriter outputStreamWriter=new OutputStreamWriter(fileOutputStream,"UTF-8");
+        BufferedWriter bufferedWriter=new BufferedWriter(outputStreamWriter);
+        bufferedWriter.write("characters:"+charactersNum+"\nlines:"+linesNum);
+        String word="";
+        int i = 0;
+        for (Map.Entry<String,Integer>map:wordsMap)
+        {
+            word += map.getKey()+":"+map.getValue()+"\n";
+            i++;
+            if(i == 10)
+            {
+                break;
+            }
+        }
+        bufferedWriter.write("\n"+word);
+        bufferedWriter.flush();
     }
 }
