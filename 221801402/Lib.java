fc13d0c8b1a7fa03b51b686a2de7d6273ed50de5
noHAHA123 <2498241730@qq.com>
2021-03-05 19:35:42 +0800

    改注释

--- 221801402/Lib.java
+++ 221801402/Lib.java
@@ -5,47 +5,49 @@ import java.io.FileWriter;
 import java.util.*;
 
 public class Lib {
-        public int CountChar(String inpath) {
-            File file = new File(inpath);
-            int i = 0;
-            try {
-                String name = file.getName();
-                System.out.println("File:" + name);
-                FileReader fr = new FileReader(file);
-                BufferedReader bufr = new BufferedReader(fr);
-                while(((char) bufr.read()) != (char)-1){
-                    i++;//按字符读取文本内容
-                }
-                bufr.close();
-                fr.close();
+    //计算字符数
+    public int CountChar(String inpath) {
+        File file = new File(inpath);
+        int i = 0;
+        try {
+            String name = file.getName();
+            System.out.println("File:" + name);
+            FileReader fr = new FileReader(file);
+            BufferedReader bufr = new BufferedReader(fr);
+            while(((char) bufr.read()) != (char)-1){
+                i++;//按字符读取文本内容
             }
-            catch(Exception e)
-            {
-                e.printStackTrace();
-            }
-            return i;
+            bufr.close();
+            fr.close();
         }
-
-        public int CountLine(String inpath){
-            File file = new File(inpath);
-            int i = 0;
-            try {
-                String name = file.getName();
-                System.out.println("File:" + name);
-                FileReader fr = new FileReader(file);
-                BufferedReader bufr = new BufferedReader(fr);
-                while (bufr.readLine() != null) {
-                    i++;
-                }
-                bufr.close();
-                fr.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return i;
+        catch(Exception e)
+        {
+            e.printStackTrace();
         }
+        return i;
+    }
 
+    //计算行数
+    public int CountLine(String inpath){
+        File file = new File(inpath);
+        int i = 0;
+        try {
+            String name = file.getName();
+            System.out.println("File:" + name);
+            FileReader fr = new FileReader(file);
+            BufferedReader bufr = new BufferedReader(fr);
+            while (bufr.readLine() != null) {
+                i++;
+            }
+            bufr.close();
+            fr.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return i;
+    }
 
+    //计算总单词数，统计单词词频
     public void CountWord(String inpath,String outpath,int charNum,int lineNum) {
         File file = new File(inpath);
         try {
@@ -55,6 +57,7 @@ public class Lib {
             String line = null;
             String regex = "[a-zA-Z]{4,}[a-zA-Z0-9]*";
             Map<String, Integer> map = new HashMap<String, Integer>();
+            //逐行读取，分割匹配单词
             while ((line = bufr.readLine()) != null) {
                 line = line.toLowerCase();
                 String[] words=line.split("[^(a-zA-Z0-9)]+");
@@ -72,6 +75,7 @@ public class Lib {
             bufr.close();
             fr.close();
 
+            //排序，值相同的根据字典序排序
             ArrayList<HashMap.Entry<String, Integer>> list =
                     new ArrayList<HashMap.Entry<String, Integer>>(map.entrySet());
             Collections.sort(list, new Comparator<HashMap.Entry<String, Integer>>() {
@@ -91,8 +95,10 @@ public class Lib {
                     }
                 }
             });
+
             int n = list.size();
             System.out.println("一共有" + n + "种单词");
+            //输出到目标文件
             out.write("characters:"+charNum+"\n");
             out.write("words:"+n+"\n");
             out.write("lines:"+lineNum+"\n");
