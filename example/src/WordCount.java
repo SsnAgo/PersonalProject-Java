b9c600c769563eaa29864e44f6673c9ac5f42a70
221801333lsl <1390476733@qq.com>
2021-03-05 20:57:14 +0800

    Update WordCount.java
    实现了map不区分大小写，同时实现了优先值value排序，若value相同时对键key按字母表顺序排序

--- example/src/WordCount.java
+++ example/src/WordCount.java
@@ -3,9 +3,15 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.security.KeyStore.Entry;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
 
 public class Count {
@@ -47,20 +53,36 @@ public class Count {
         	String[]ss = str.split("\\s+|\\W");
         	for(String s:ss)
 			{
-        		if(s.matches("[a-z,A-Z]{4,}.*")) {
-        		if(map.containsKey(s))
-					map.put(s, map.get(s)+1);
-				else
-					map.put(s, 1);
+        		if(s.matches("[a-z,A-Z]{4,}.*")) 
+        		{
+	        		if(map.containsKey(s.toLowerCase()))        			
+						map.put(s.toLowerCase(), map.get(s.toLowerCase())+1);
+					else
+						map.put(s.toLowerCase(), 1);
         		}
 			}
         }
-        //输出所有单词数
-        Set<String>keys = map.keySet();
-        for(String key : keys)
-        	System.out.println(key + "有：" + map.get(key) + "个.");
+        
+        List<String> result = new ArrayList<>();
+        List<Map.Entry<String,Integer>> list = new ArrayList<>();
+        list.addAll(map.entrySet());
+        Collections.sort(list,new Comparator<Map.Entry<String,Integer>>(){
+            public int compare(Map.Entry<String,Integer>e1,Map.Entry<String,Integer>e2){
+                int re = e2.getValue().compareTo(e1.getValue());
+                if(re!=0){return re;}
+                else{return e1.getKey().compareTo(e2.getKey());}
+            }
+        });
+        for(int i=0;i<map.size();i++){
+            result.add(list.get(i).getKey());
+        }
+        for(int i=0;i<10;i++){
+            System.out.println(list.get(i));
+        }          
         System.out.print(count_line);
         System.out.print(count_char);
         fis.close();
 	}
+	
 }
+
