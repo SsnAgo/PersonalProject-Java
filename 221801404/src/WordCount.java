8bd947cc651fb452d6d0b60e63f0368d30bca806
weihaochen111 <786576329@qq.com>
2021-03-05 10:53:37 +0800

    对文件读取组一些极限测试

--- 221801404/src/WordCount.java
+++ 221801404/src/WordCount.java
@@ -8,8 +8,26 @@ public class WordCount
         String filePath = args[0];
         String content = Lib.readFile(filePath);
         FileWriter writer;
+        //对文件字符的读取做测试，数据量过于巨大时，出现下面问题
+        //并且在运行期间画面都不太稳定
+        /*java.io.IOException: 磁盘空间不足。
+	at java.base/java.io.FileOutputStream.writeBytes(Native Method)
+	at java.base/java.io.FileOutputStream.write(FileOutputStream.java:347)
+	at java.base/sun.nio.cs.StreamEncoder.writeBytes(StreamEncoder.java:242)
+	at java.base/sun.nio.cs.StreamEncoder.implWrite(StreamEncoder.java:312)
+	at java.base/sun.nio.cs.StreamEncoder.implWrite(StreamEncoder.java:290)
+	at java.base/sun.nio.cs.StreamEncoder.write(StreamEncoder.java:131)
+	at java.base/sun.nio.cs.StreamEncoder.write(StreamEncoder.java:141)
+	at java.base/java.io.OutputStreamWriter.write(OutputStreamWriter.java:226)
+	at java.base/java.io.Writer.write(Writer.java:249)
+	at WordCount.main(WordCount.java:15)
+*/
         try {
             writer = new FileWriter(args[1]);
+            for(int i = 0;i < 100000;i++)
+            {
+                writer.write(content);
+            }
             writer.write(content);
             writer.flush();
             writer.close();
