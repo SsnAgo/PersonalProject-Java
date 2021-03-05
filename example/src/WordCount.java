129b91548a24d8796780f9c5653a340d768ef790
221801333lsl <1390476733@qq.com>
2021-03-05 19:41:51 +0800

    Update WordCount.java
    完成了用空格或者非字母数字分隔字符串，并对单词开头四个字母进行判断。

--- example/src/WordCount.java
+++ example/src/WordCount.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Set;
 
 public class Count {
 	//定义三个函数，分别用来将字符数、单词数、有效行数、十个词频最高的单词写入文件
@@ -34,12 +36,28 @@ public class Count {
         int count_char=0;//用于统计总字符数 
         int count_line=0;
         String regxSpace = "\\s*";
+        Map<String,Integer> map = new HashMap<String,Integer>();
         while ((str = in.readLine())!= null) 
         {//readLine()方法, 用于读取一行,只要读取内容不为空就一直执行   	
         	count_char += str.length();  
+        	//当不为空行时，行数加一
         	if(!str.matches(regxSpace))
         		count_line++;
+        	//统计单词数并统计单词出现次数
+        	String[]ss = str.split("\\s+|\\W");
+        	for(String s:ss)
+			{
+        		if(s.matches("[a-z,A-Z]{4,}.*")) {
+        		if(map.containsKey(s))
+					map.put(s, map.get(s)+1);
+				else
+					map.put(s, 1);
+        		}
+			}
         }
+        Set<String>keys = map.keySet();
+        for(String key : keys)
+        	System.out.println(key + "有：" + map.get(key) + "个.");
         System.out.print(count_line);
         System.out.print(count_char);
         fis.close();
