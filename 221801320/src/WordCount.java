9fefc9bc01ade4dc674541544c3a96fb20a62be4
luogugu <49429994+luogugu@users.noreply.github.com>
2021-03-05 21:13:19 +0800

    Add files via upload

--- 221801320/src/WordCount.java
+++ 221801320/src/WordCount.java
@@ -7,8 +7,8 @@ public class WordCount {
 	
 	public static void main(String[] args) throws IOException {
 		
-        String inputFile = "F:\\1.txt";
-        String outputFile = "F:\\2.txt";
+        String inputFile = args[0];
+        String outputFile = args[1];
         Lib.charactersCount(inputFile,outputFile);//统计文件字符数量
         Lib.wordsCount(inputFile,outputFile);//统计文件单词数
         Lib.linesCount(inputFile,outputFile);//统计文件有效行数
