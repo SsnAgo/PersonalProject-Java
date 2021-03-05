0967cc0c1f2df3011714e53bd6d99b68f25207e4
ljl <1119583809@qq.com>
2021-03-05 21:21:03 +0800

    修改.gitignore并进行接口封装

---
+++ 221801139/src/Lib.java
@@ -0,0 +1,43 @@
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Lib {
+    public static int CountEachLineCharacter(String line) {
+        return line.length();
+    }
+
+    public static int CountWordNum(String line, List<String> lists) {
+        int wordcount = 0;
+        String[] wordsArr1 = line.split("[^a-zA-Z0-9]");  //切割筛选单词
+
+        for (String newword : wordsArr1) {  
+            if(newword.length() != 0) {               	
+                if((newword.length() >= 4) && (Character.isLetter(newword.charAt(0))
+                    && Character.isLetter(newword.charAt(1)) && Character.isLetter(newword.charAt(2))
+                    && Character.isLetter(newword.charAt(3)))) {               		
+                    wordcount++;
+                    lists.add(newword);  
+                }
+            }  
+        }
+
+        return wordcount;
+    }
+
+    public static void CountEachWordNum(Map<String, Integer> wordsCount,List<String> lists) {
+ 
+          
+        for (String li : lists) {  //单词出现个数统计
+            if(wordsCount.get(li) != null) { 
+                wordsCount.put(li,wordsCount.get(li) + 1);  
+            }
+            else {  
+                wordsCount.put(li,1);  
+            }  
+  
+        }  
+    }
+
+
+}
--- 221801139/src/WordCount.java
+++ 221801139/src/WordCount.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,9 +10,8 @@ public class WordCount {
   
     public static void main(String[] args) throws Exception {  
         String inputfile = args[0];
-    	    	
+    
     	BufferedReader br = new BufferedReader(new FileReader(inputfile));  
-    	
     	int characterscount=0;
         int wordline = 0;
         int wordcount = 0;
@@ -22,34 +20,15 @@ public class WordCount {
 		while((readLine = br.readLine()) != null) {  
 			wordline++;
             readLine = readLine.toLowerCase();
-            String[] wordsArr1 = readLine.split("[^a-zA-Z0-9]");  //切割筛选单词
             
-            characterscount+=readLine.length();//统计每行的字符数
+            characterscount += Lib.CountEachLineCharacter(readLine);//统计每行的字符数
                         
-            for (String newword : wordsArr1) {  
-                if(newword.length() != 0) {               	
-                	if((newword.length()>=4)&&(Character.isLetter(newword.charAt(0))
-                        &&Character.isLetter(newword.charAt(1))&&Character.isLetter(newword.charAt(2))
-                        &&Character.isLetter(newword.charAt(3)))) {               		
-                        wordcount++;
-                        lists.add(newword);  
-                	}
-                }  
-            }  
+            wordcount += Lib.CountWordNum(readLine, lists);
         }           
         br.close();  
           
-        Map<String, Integer> wordsCount = new TreeMap<String,Integer>();  
-          
-        for (String li : lists) {  //单词出现个数统计
-            if(wordsCount.get(li) != null) { 
-                wordsCount.put(li,wordsCount.get(li) + 1);  
-            }
-            else {  
-                wordsCount.put(li,1);  
-            }  
-  
-        }  
+        Map<String, Integer> wordsCount = new TreeMap<String,Integer>(); 
+        Lib.CountEachWordNum(wordsCount, lists);
           
         Print p = new Print(args[1]);
         p.SortMap(wordsCount,wordline,wordcount,characterscount+wordline);  //排序并输出
