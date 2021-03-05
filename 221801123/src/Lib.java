890d9b753d6e48d1261ceaebf5b16e46dee5b3a4
221801123 <1041618073@qq.com>
2021-03-05 18:36:25 +0800

    correct codestyle

--- 221801123/src/Lib.java
+++ 221801123/src/Lib.java
@@ -3,73 +3,83 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * 统计工具
+ * @description 统计工具类
  */
 class Lib {
+    /**
+     * @description 存放单词与其对应出现的次数
+     */
+    static private HashMap<String, Integer> wordMap = null;
 
-	//存放单词与其对应出现的次数
-	static private HashMap<String, Integer> mp = null;
+    /**
+     * @param str 文件字符串
+     * @return 字符数
+     * @description 统计字符数
+     */
+    static int countCharNum(String str) {
+        return str.length();
+    }
 
-	/**
-	 * 统计总字符数
-	 */
-	static int countCharNum(String str){
-		return str.length();
-	}
+    /**
+     * @param str 文件字符串
+     * @return 有效行数
+     * @description 统计空白行数
+     */
+    static int countValidLineNum(String str) {
+        int lineNum = 0;
+        Pattern linePattern = Pattern.compile("(^|\n)\\s*\\S+");
+        Matcher matcher = linePattern.matcher(str);
 
-	/**
-	 * 统计空白行数
-	 */
-	static int countValidLineNum(String str) {
-		int lineNum = 0;
-		Pattern linePattern = Pattern.compile("(^|\n)\\s*\\S+");
-		Matcher matcher = linePattern.matcher(str);
-		while (matcher.find()) {
-			lineNum++;
-		}
-		return lineNum;
-	}
+        while (matcher.find())
+            lineNum++;
 
-	/**
-	 * 统计单词数,并统计单词对应个数
-	 */
-	static int countWordNum(String str) {
-		//单词数
-		int wordNum = 0;
-		//全部默认为小写
-		str = str.toLowerCase();
-		String[] words = str.split("[^a-z0-9]+");
-		mp = new HashMap<>();
-		for (String word : words) {
-			if (word.matches("[a-z]{4,}[a-z0-9]*")) {
-				wordNum++;
-				if (mp.containsKey(word)){
-					int n = mp.get(word);
-					mp.put(word, n+1);
-				}
-				else {
-					mp.put(word, 1);
-				}
-			}
-		}
-		return wordNum;
-	}
+        return lineNum;
+    }
 
-	/**
-	 * 对单词频率进行排序
-	 */
-	static List<HashMap.Entry<String, Integer>> sortWordMap(){
-		List<HashMap.Entry<String, Integer>> wordsList = new ArrayList<>(mp.entrySet());
-		Collections.sort(wordsList, new Comparator<HashMap.Entry<String, Integer>>() {
-			@Override
-			public int compare(HashMap.Entry<String, Integer> word1, HashMap.Entry<String, Integer> word2) {
-				if(word1.getValue().equals(word2.getValue())) {
-					return word1.getKey().compareTo(word2.getKey());
-				} else {
-					return word2.getValue() - word1.getValue();
-				}
-			}
-		});
-		return wordsList;
-	}
+    /**
+     * @param str 文件字符串
+     * @return 单词总数
+     * @description 统计单词总数, 并统计单词对应个数
+     */
+    static int countWordNum(String str) {
+        int wordNum = 0;
+        str = str.toLowerCase();
+        String[] words = str.split("[^a-z0-9]+");
+        wordMap = new HashMap<>();
+
+        for (String word : words) {
+            if (word.matches("[a-z]{4,}[a-z0-9]*")) {
+                wordNum++;
+                if (wordMap.containsKey(word)) {
+                    int n = wordMap.get(word);
+                    wordMap.put(word, n + 1);
+                } else {
+                    wordMap.put(word, 1);
+                }
+            }
+        }
+
+        return wordNum;
+    }
+
+    /**
+     * @return 排序后的wordMapList
+     * @description 对wordMap中的单词频率排序
+     */
+    static List<HashMap.Entry<String, Integer>> sortWordMap() {
+        List<HashMap.Entry<String, Integer>> wordMapList = new ArrayList<>(wordMap.entrySet());
+
+        Collections.sort(wordMapList, new Comparator<HashMap.Entry<String, Integer>>() {
+            @Override
+            public int compare(HashMap.Entry<String, Integer> word1, HashMap.Entry<String, Integer> word2) {
+                if (word1.getValue().equals(word2.getValue())) {
+                    return word1.getKey().compareTo(word2.getKey());
+                } else {
+                    return word2.getValue() - word1.getValue();
+                }
+            }
+        });
+
+        return wordMapList;
+    }
 }
--- 221801123/src/WordCount.java
+++ 221801123/src/WordCount.java
@@ -6,90 +6,103 @@ import java.util.HashMap;
 import java.util.List;
 
 public class WordCount {
+    /**
+     * @description 文件处理工具类
+     */
+    static class IOUtils {
+        /**
+         * @param infile 待读取文件的地址
+         * @return 文件字符串
+         * @throws IOException
+         * @description 读取指定文件，返回对应字符串形式
+         */
+        static String readFile(String infile) throws IOException {
+            BufferedReader reader = null;
+            StringBuilder builder = new StringBuilder();
+            int ch = 0;
 
-	/**
-	 * 文件处理工具类
-	 */
-	static class IOUtil{
-		/**
-		 * 读取指定文件，返回对应字符串形式
-		 */
-		static String readFile(String infile) throws IOException {
-			BufferedReader reader = null;
-			StringBuilder builder = new StringBuilder();
-			int ch = 0;
-			try {
-				reader = new BufferedReader(new FileReader(infile));
-				while ((ch = reader.read()) != -1) {
-					builder.append((char)ch);
-				}
-			} catch (IOException e) {
-				System.out.println("文件读取错误");
-				throw e;
-			} finally {
-				if (reader != null)
-					reader.close();
-			}
-			return builder.toString();
-		}
+            try {
+                reader = new BufferedReader(new FileReader(infile));
+                while ((ch = reader.read()) != -1) {
+                    builder.append((char) ch);
+                }
+            } catch (IOException e) {
+                System.out.println("文件读取错误");
+                throw e;
+            } finally {
+                if (reader != null)
+                    reader.close();
+            }
 
-		/**
-		 * 写文件
-		 */
-		static void writeFile(String result, String outfile) throws IOException {
-			BufferedWriter writer = null;
-			try {
-				writer = Files.newBufferedWriter(Paths.get(outfile), StandardCharsets.UTF_8);
-				writer.write(result);
-			} catch (IOException e) {
-				System.out.println("文件写入错误");
-				throw e;
-			} finally {
-				if (writer != null)
-					writer.close();
-			}
-		}
-	}
+            return builder.toString();
+        }
 
-	/**
-	 * 输出内容硬编码
-	 */
-	private String getResult(String content){
-		StringBuilder result = new StringBuilder("characters: " + Lib.countCharNum(content) + "\n"
-				+ "words: " + Lib.countWordNum(content) + "\n"
-				+ "lines: " + Lib.countValidLineNum(content) + "\n");
-		List<HashMap.Entry<String, Integer>> wordsList = Lib.sortWordMap();
-		int count = 0;
-		for (HashMap.Entry entry: wordsList) {
-			count++;
-			result.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
-			if (count >= 10)
-				break;
-		}
-		return result.toString();
-	}
+        /**
+         * @param result  统计数据字符串
+         * @param outfile 待写入文件的地址
+         * @throws IOException
+         * @description 将统计数据字符串写入文件
+         */
+        static void writeFile(String result, String outfile) throws IOException {
+            BufferedWriter writer = null;
 
-	/**
-	 * 统计
-	 */
-	private void process(String infile, String outfile) throws IOException {
-		String content = IOUtil.readFile(infile);
-		IOUtil.writeFile(getResult(content),outfile);
+            try {
+                writer = Files.newBufferedWriter(Paths.get(outfile), StandardCharsets.UTF_8);
+                writer.write(result);
+            } catch (IOException e) {
+                System.out.println("文件写入错误");
+                throw e;
+            } finally {
+                if (writer != null)
+                    writer.close();
+            }
+        }
+    }
 
-	}
+    /**
+     * @param content 文件字符串
+     * @return 统计数据字符串
+     * @description 统计数据硬编码
+     */
+    private String getResult(String content) {
+        StringBuilder result = new StringBuilder("characters: " + Lib.countCharNum(content) + "\n" + "words: "
+            + Lib.countWordNum(content) + "\n" + "lines: " + Lib.countValidLineNum(content) + "\n");
+        List<HashMap.Entry<String, Integer>> wordsList = Lib.sortWordMap();
+        int count = 0;
 
-	public static void main(String[] args) {
-		if (args.length < 2) {
-			System.out.println("2 paths needed");
-			return;
-		} else if (args.length > 2)
-			System.out.println("choose tow font paths");
+        for (HashMap.Entry entry : wordsList) {
+            count++;
+            result.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
+            if (count >= 10)
+                break;
+        }
 
-		try {
-			new WordCount().process(args[0], args[1]);
-		} catch (IOException e){
-			System.out.println("出错啦");
-			e.printStackTrace();
-		}
-	}
+        return result.toString();
+    }
+
+    /**
+     * @param infile  待读取文件的地址
+     * @param outfile 待写入文件的地址
+     * @throws IOException
+     * @description 执行统计
+     */
+    private void process(String infile, String outfile) throws IOException {
+        String content = IOUtils.readFile(infile);
+        IOUtils.writeFile(getResult(content), outfile);
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("2 paths needed");
+            return;
+        } else if (args.length > 2)
+            System.out.println("choose tow font paths");
+
+        try {
+            new WordCount().process(args[0], args[1]);
+        } catch (IOException e) {
+            System.out.println("出错啦");
+            e.printStackTrace();
+        }
+    }
 }
