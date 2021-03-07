df3f3d386e514c46b3017bbccde7102ca1b9f00e
lauhiukwan <54564794+lauhiukwan@users.noreply.github.com>
2021-03-07 20:51:14 +0800

    Update WordCount.java

--- 221801412/src/WordCount.java
+++ 221801412/src/WordCount.java
@@ -1,4 +1,4 @@
-import java.io.*;
+iimport java.io.*;
 import java.util.*;
 
 public class WordCount {
@@ -10,29 +10,26 @@ public class WordCount {
 	    File output;
 	    //若命令行参数小于2则新建文件
 	    if (args.length < 2) {
-	        input = new File("src/input.txt");
-	        output = new File("src/output.txt");
+	        input = new File("221801412/src/input.txt");
+	        output = new File("221801412/src/output.txt");
         }
         else {
             input = new File(args[0]);
             output = new File(args[1]);
         }
+        CountData data = new CountData();
+        Lib.countChar(input);
+        Lib.openFile(input);
+        data = Lib.getData();
+        int countChar = data.getCountChar();
+        int countLine = data.getCountLine();
+        int countWord = data.getCountWord();
+        List<Map.Entry<String, Integer>> getWordFrequency = data.getGetWordFrequency();
         try {
-            CountData data = new CountData();
-            Lib.openFile(input);
-            Lib.countChar(input);
-            data = Lib.getData();
-            //统计字符数
-            int countChar = data.getCountChar();
-            //统计行数
-            int countLine = data.getCountLine();
-            //统计单词数
-            int countWord = data.getCountWord();
-            List<Map.Entry<String, Integer>> getWordFrequency = data.getGetWordFrequency();
             BufferedWriter writer = new BufferedWriter(new FileWriter(output));
-            writer.write("characters: " + countChar + "\n");
-            writer.write("words: " + countWord + "\n");
-            writer.write("lines: " + countLine + "\n");
+            writer.write("characters:" + countChar + "\n");
+            writer.write("words:" + countWord + "\n");
+            writer.write("lines:" + countLine + "\n");
             if (getWordFrequency.size() < 10) {
                 for (int i = 0; i < getWordFrequency.size(); i++) {
                     writer.write(getWordFrequency.get(i).getKey() + ": " + getWordFrequency.get(i).getValue());
